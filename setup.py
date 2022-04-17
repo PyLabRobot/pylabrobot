214ef63a02954d4dd73ef222a3d8a3350f06e8a0
Stefan Golas <32437678+stefangolas@users.noreply.github.com>
2022-04-16 22:38:48 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 try:
     import pypandoc
     long_description = pypandoc.convert_file('README.md', 'rst')
+    print(long_description)
 except(IOError, ImportError):
     long_description = open('README.md').read()
 
