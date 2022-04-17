09bc8cda292bbaf04ddc13490507b25dd3afc1cf
Stefan Golas <32437678+stefangolas@users.noreply.github.com>
2022-04-16 22:31:01 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     packages=find_packages(exclude=['tests*', 'examples*']),
     license='MIT',
     description='Python for Hamilton liquid handling robots',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     install_requires=['requests', 'pythonnet', 'pywin32', 'pyserial'],
     package_data={'pyhamilton': ['star-oem/*', 'star-oem/VENUS_Method/*']},
