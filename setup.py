5480e98e3dc50ff6a4c99291e891ce8698f42bc8
Stefan Golas <32437678+stefangolas@users.noreply.github.com>
2022-04-16 22:30:26 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from setuptools import setup, find_packages
 
+try:
+    import pypandoc
+    long_description = pypandoc.convert_file('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+
 setup(
     name='pyhamilton',
     version='1.235',
