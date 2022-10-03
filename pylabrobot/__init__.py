cfd17694a2206004691f4d4ec62e337ad7cecf8b
Rick Wierenga <rick_wierenga@icloud.com>
2022-10-03 15:37:51 -0400

    logging

--- pylabrobot/__init__.py
+++ pylabrobot/__init__.py
@@ -1,3 +1,12 @@
+import datetime
 import logging
-logging.basicConfig(level=logging.INFO)
 
+# Create a logger
+logger = logging.getLogger(__name__)
+
+# Add a file handler
+now = datetime.datetime.now().strftime("%d%m%Y")
+fh = logging.FileHandler(f"pylabrobot-{now}.log")
+fh.setLevel(logging.DEBUG)
+fh.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
+logger.addHandler(fh)
