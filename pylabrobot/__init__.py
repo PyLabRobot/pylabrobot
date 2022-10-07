ed08dc38e02fa4290177e9beede9e543c47a6746
Rick Wierenga <rick_wierenga@icloud.com>
2022-10-07 12:16:30 -0400

    Sane date format

--- pylabrobot/__init__.py
+++ pylabrobot/__init__.py
@@ -5,7 +5,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 # Add a file handler
-now = datetime.datetime.now().strftime("%d%m%Y")
+now = datetime.datetime.now().strftime("%Y%m%d")
 fh = logging.FileHandler(f"pylabrobot-{now}.log")
 fh.setLevel(logging.DEBUG)
 fh.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
