88ffeaa323dc1145f1b9dd0d780588cdb73d3c84
Rick Wierenga <rick_wierenga@icloud.com>
2023-01-18 12:01:46 -0500

    Add trough

---
+++ pylabrobot/resources/trough.py
@@ -0,0 +1,29 @@
+from typing import Optional
+
+from .container import Container
+
+
+class Trough(Container):
+  """ A trough is a container, particularly useful for multichannel liquid handling operations. """
+
+  def __init__(
+    self,
+    name: str,
+    size_x: float,
+    size_y: float,
+    size_z: float,
+    max_volume: float,
+    volume: float = 0,
+    category: Optional[str] = "trough",
+    model: Optional[str] = None
+  ):
+    super().__init__(
+      name=name,
+      size_x=size_x,
+      size_y=size_y,
+      size_z=size_z,
+      max_volume=max_volume,
+      volume=volume,
+      category=category,
+      model=model
+    )
