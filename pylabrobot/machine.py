48a06d96577b4ac525b46b84af068a4c3862db6c
Stefan Golas <stefanmgolas@gmail.com>
2023-09-30 10:56:05 -0400

    Fixed MachineFrontend.stop() was not awaited

--- pylabrobot/machine.py
+++ pylabrobot/machine.py
@@ -56,11 +56,11 @@ class MachineFrontend(ABC):
 
   def __del__(self):
     if self.setup_finished:
-      self.stop()
+      await self.stop()
 
   def __enter__(self):
     self.setup()
     return self
 
   def __exit__(self, exc_type, exc_value, traceback):
-    self.stop()
+    await self.stop()
