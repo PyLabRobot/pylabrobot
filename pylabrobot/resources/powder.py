d0281bcb3a4a63e1bcbaf8ae4ddccb577fed47c2
Valerijs Korotkovs <valerijs.korotkovs@chemspeed.com>
2023-12-19 11:15:08 +0100

    Powder Dispenser: Initial commit
    Initial commit to include front end class for a powder dispenser, abstract class for a backend and a powder resource.

---
+++ pylabrobot/resources/powder.py
@@ -0,0 +1,11 @@
+class Powder:
+  """
+  A class for powders.
+  """
+  def __init__(self, name: str) -> None:
+    """
+    Initializes a new instance of the Powder class.
+    Args:
+        name (str): The name of the powder.
+    """
+    self.name = name
