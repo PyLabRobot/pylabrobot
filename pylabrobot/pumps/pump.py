0bdba0c188a7b944edca0c4e1e78e29f77098699
Rick Wierenga <rick_wierenga@icloud.com>
2023-12-22 12:01:39 +0100

    Add pumps documentation

--- pylabrobot/pumps/pump.py
+++ pylabrobot/pumps/pump.py
@@ -10,10 +10,18 @@ class Pump(MachineFrontend):
     self.backend: PumpBackend = backend
 
   def run_revolutions(self, num_revolutions: float):
+    """ Run a given number of revolutions. This method will return after the command has been sent,
+    and the pump will run until `halt` is called.
+
+    Args:
+      num_revolutions: number of revolutions to run
+    """
+
     self.backend.run_revolutions(num_revolutions=num_revolutions)
 
   def run_continuously(self, speed: float):
-    """ Run continuously at a given speed.
+    """ Run continuously at a given speed. This method will return after the command has been sent,
+    and the pump will run until `halt` is called.
 
     If speed is 0, the pump will be halted.
 
@@ -24,4 +32,6 @@ class Pump(MachineFrontend):
     self.backend.run_continuously(speed=speed)
 
   def halt(self):
+    """ Halt the pump immediately. """
+
     self.backend.halt()
