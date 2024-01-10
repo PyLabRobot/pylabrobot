6525d142f84e21513b9b7f58963ff0f2fca4e9ab
Camillo Moschner <camillo.moschner@biocam.guide>
2024-01-10 15:38:14 +0000

    TTx labware sharing polishing

--- pylabrobot/resources/TTx_DIY/README.md
+++ pylabrobot/resources/TTx_DIY/README.md
@@ -7,4 +7,4 @@
 
 | Description               | Image              |
 |--------------------|--------------------|
-| 'TTx_EppiPlate'<br>Part no.: 6008280<br>[manufacturer website](https://www.perkinelmer.com/uk/Product/proxiplate-384-plus-50w-6008280) | <img src="ims/revvity_ProxiPlate-384-Plus-White-384-shallow-well-Microplate.jpg" alt="TTx_EppiPlate" width="250"/> |
+| 'TTx_EppiPlate'<br>Part no.: TTx_OpenSource_000<br>[manufacturer website](https://tbd) | <img src="ims/TTx_24_EppiPlate.png" alt="TTx_EppiPlate" width="250"/> |
--- pylabrobot/resources/TTx_DIY/plates.py
+++ pylabrobot/resources/TTx_DIY/plates.py
@@ -10,9 +10,9 @@ from pylabrobot.resources.itemized_resource import create_equally_spaced
 from pylabrobot.resources.volume_functions import *
 
 
-def _compute_volume_from_height_TTx_EppiPlate(h: float):
+def _compute_volume_from_height_TTx_24_EppiPlate(h: float):
   if h > 77:
-    raise ValueError(f"Height {h} is too large for TTx_EppiPlate")
+    raise ValueError(f"Height {h} is too large for TTx_24_EppiPlate")
   return calculate_liquid_volume_container_2segments_round_Vb( # Simplification: instead of 3 segment (hemisphere-frustum of cone-cylinder) -> 2 segment (cone-cylinder)
     d=9,
     h_cone = 18, 
@@ -21,8 +21,8 @@ def _compute_volume_from_height_TTx_EppiPlate(h: float):
     )
 
 
-#: TTx_EppiPlate (DIY (3D-printed) tube rack holding 24x 1.5mL Eppendorf tubes)
-def TTx_EppiPlate(name: str, with_lid: bool = False) -> Plate:
+#: TTx_24_EppiPlate (DIY (3D-printed) tube rack holding 24x 1.5mL Eppendorf tubes)
+def TTx_24_EppiPlate(name: str, with_lid: bool = False) -> Plate:
   # TODO: Add link to STL file once website for download is set up
   return Plate(
     name=name,
@@ -30,7 +30,7 @@ def TTx_EppiPlate(name: str, with_lid: bool = False) -> Plate:
     size_y=86.0,
     size_z=41.9,
     with_lid=with_lid,
-    model="TTx_EppiPlate",
+    model="TTx_24_EppiPlate",
     lid_height=0,
     items=create_equally_spaced(Well,
         num_items_x=6,
@@ -44,25 +44,7 @@ def TTx_EppiPlate(name: str, with_lid: bool = False) -> Plate:
         size_y=9,
         size_z=38,
       bottom_type=WellBottomType.U,
-      compute_volume_from_height=_compute_volume_from_height_TTx_EppiPlate,
+      compute_volume_from_height=_compute_volume_from_height_TTx_24_EppiPlate,
     ),
   )
 
-# # DIY (3D-printed) tube rack holding 24x 1.5mL Eppendorf tubes
-# class EppiPlate(ItemizedResource[Eppi_tube]):
-#   def __init__(self, name: str):
-#     super().__init__(
-#       name=name,
-#       size_x=127.0,
-#       size_y=86.0,
-#       size_z=41.9,
-#       items=create_equally_spaced(Eppi_tube,
-#         num_items_x=6,
-#         num_items_y=4,
-#         dx=19.574,
-#         dy=9.09,
-#         dz=5.0,
-#         item_dx=19.5,
-#         item_dy=19.5,
-#       )
-#     )
