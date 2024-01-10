c65e97380a959dad57ebb29adcb3c88357705f3a
Camillo Moschner <camillo.moschner@biocam.guide>
2024-01-10 15:27:31 +0000

    adding volume_functions, axygen resources, TTx_DIY resources

---
+++ pylabrobot/resources/TTx_DIY/README.md
@@ -0,0 +1,10 @@
+
+## Resource defintions: T-Therapeutics
+
+> T-Therapeutics Ltd. is a Cambridge(UK)-based biotech startup founded in 2022. It values innovation and openness and has decided to share some of the designs for automation-specific labware it generates with the PyLabRobot community (under the same MIT open-source licencing).
+
+### Currently defined labware:
+
+| Description               | Image              |
+|--------------------|--------------------|
+| 'TTx_EppiPlate'<br>Part no.: 6008280<br>[manufacturer website](https://www.perkinelmer.com/uk/Product/proxiplate-384-plus-50w-6008280) | <img src="ims/revvity_ProxiPlate-384-Plus-White-384-shallow-well-Microplate.jpg" alt="TTx_EppiPlate" width="250"/> |
---
+++ pylabrobot/resources/corning_axygen/plates.py
@@ -0,0 +1,60 @@
+""" Corning Axygen plates """
+
+# pylint: disable=invalid-name
+
+from pylabrobot.resources.container import Container
+from pylabrobot.resources.well import Well, WellBottomType
+from pylabrobot.resources.itemized_resource import create_equally_spaced
+from pylabrobot.resources.plate import Plate
+
+from pylabrobot.resources.volume_functions import *
+
+
+
+def _compute_volume_from_height_Axy_24_DW_10ML(h: float):
+  if h > 42.1:
+    raise ValueError(f"Height {h} is too large for Cos_96_Vb")
+  return calculate_liquid_volume_container_2segments_square_Vb(
+    x=17,
+    y=17,
+    z_pyramid=5,
+    z_cube=37,
+    liquid_height=h)
+
+
+#: Axy_24_DW_10ML
+def Axy_24_DW_10ML(name: str, with_lid: bool = False) -> Plate:
+  return Plate(
+    name=name,
+    size_x=127.0,
+    size_y=86.0,
+    size_z=44.24,
+    with_lid=with_lid,
+    model="Axy_24_DW_10ML",
+    lid_height=5,
+    items=create_equally_spaced(Container, # Well
+      num_items_x=6,
+      num_items_y=4,
+      dx=10.3,
+      dy=5.8,
+      dz=2.24,
+      item_dx= 18,
+      item_dy=18, 
+      size_x=17.0,
+      size_y=17.0,
+      size_z=42,
+      # bottom_type=WellBottomType.V,
+      # compute_volume_from_height=_compute_volume_from_height_Axy_24_DW_10ML,
+    ),
+  )
+
+
+#: Axy_24_DW_10ML_L
+def Axy_24_DW_10ML_L(name: str, with_lid: bool = False) -> Plate:
+  return Axy_24_DW_10ML(name=name, with_lid=with_lid)
+
+
+#: Axy_24_DW_10ML_P
+def Axy_24_DW_10ML_P(name: str, with_lid: bool = False) -> Plate:
+  return Axy_24_DW_10ML(name=name, with_lid=with_lid).rotated(90)
+
