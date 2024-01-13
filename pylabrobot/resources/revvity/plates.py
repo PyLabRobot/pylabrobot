51d04e9c8a7ef61bcf8c68e19cff4fd9e66afaae
Camillo Moschner <camillo.moschner@biocam.guide>
2024-01-13 16:11:16 +0000

    linting checks

--- pylabrobot/resources/revvity/plates.py
+++ pylabrobot/resources/revvity/plates.py
@@ -6,14 +6,16 @@ from pylabrobot.resources.plate import Plate
 from pylabrobot.resources.well import Well, WellBottomType
 from pylabrobot.resources.itemized_resource import create_equally_spaced
 
-from pylabrobot.resources.volume_functions import calculate_liquid_volume_container_2segments_round_Vb
+from pylabrobot.resources.volume_functions import calculate_liquid_volume_container_2segments_round_vbottom
 
 
 def _compute_volume_from_height_Revvity_ProxiPlate_384Plus(h: float):
+  """ Simplification: instead of 3 segment (hemisphere-frustum of cone-cylinder) 
+  -> 2 segment (cone-cylinder)
+  """
   if h > 77:
     raise ValueError(f"Height {h} is too large for Revvity_ProxiPlate_384Plus")
-  # Simplification: instead of 3 segment (hemisphere-frustum of cone-cylinder) -> 2 segment (cone-cylinder)
-  return calculate_liquid_volume_container_2segments_round_Vb( 
+  return calculate_liquid_volume_container_2segments_round_vbottom(
     d=3.3,
     h_cone = 3,
     h_cylinder = 2.3,
