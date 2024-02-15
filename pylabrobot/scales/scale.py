b82a7c41da14544df9a053b97487abd552f23ff7
Rick Wierenga <rick_wierenga@icloud.com>
2024-02-14 19:45:29 -0500

    Scale should be a resource

--- pylabrobot/scales/scale.py
+++ pylabrobot/scales/scale.py
@@ -1,12 +1,22 @@
-from pylabrobot.machine import MachineFrontend
+from pylabrobot.machine import Machine
 from pylabrobot.scales.scale_backend import ScaleBackend
 
 
-class Scale(MachineFrontend):
+class Scale(Machine):
   """ Base class for a scale """
 
-  def __init__(self, backend: ScaleBackend):
-    super().__init__(backend=backend)
+  def __init__(self, backend: ScaleBackend, name: str = "scale"):
+    super().__init__(
+      name=name,
+      backend=backend,
+      model=backend.__class__.__name__,
+
+      # TODO:
+      size_x=0,
+      size_y=0,
+      size_z=0,
+      category="scale",
+    )
     self.backend: ScaleBackend = backend # fix type
 
   async def tare(self, **backend_kwargs):
