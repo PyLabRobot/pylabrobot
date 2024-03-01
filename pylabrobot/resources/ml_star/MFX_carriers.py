ca2e974c5353868ac43668d941a39f566d76f19b
camos95 <camillo.moschner@biocam.guide>
2024-03-01 21:03:02 +0000

    remove specialised MFX carrier setup, focus on universal needs

--- pylabrobot/resources/ml_star/MFX_carriers.py
+++ pylabrobot/resources/ml_star/MFX_carriers.py
@@ -31,26 +31,3 @@ def MFX_CAR_L5_base(name: str) -> MFXCarrier:
     ),
     model="MFX_CAR_L5_base"
   )
-
-
-def MFX_CAR_L3_2single_1triplet(name: str) -> MFXCarrier:
-  """ MFX carrier with MFX_site 0 and 1 empty and 2,3,4 occupied 
-  by the MettlerToledoWXS205SDU scale in "back" orientation, i.e. 
-  SBS-adapter is located towards the back of the machine.
-  """
-
-  return MFXCarrier(
-    name=name,
-    size_x=135.0,
-    size_y=497.0,
-    size_z=18.195,
-    sites=create_carrier_sites([
-        Coordinate(0.0, 4.5, 18.195),
-        Coordinate(0.0, 100.5, 18.195),
-        Coordinate(0.0, 196.5, 18.195),
-      ],
-      site_size_x=[135.0, 135.0, 135.0],
-      site_size_y=[94., 94.0, (94.0)*3+4],
-    ),
-    model="MFX_CAR_L_0_1_2-4"
-  )
