279327886f2ef32e13dd9fb3ce2dca2dd7a708a3
Rick Wierenga <rick_wierenga@icloud.com>
2024-03-09 14:07:13 -0500

    fix EVO collison checker & lint/type EVO tests

--- pylabrobot/liquid_handling/backends/tecan/EVO_tests.py
+++ pylabrobot/liquid_handling/backends/tecan/EVO_tests.py
@@ -13,6 +13,7 @@ from pylabrobot.liquid_handling.standard import (
   GripDirection
 )
 from pylabrobot.resources import (
+  Coordinate,
   EVO150Deck,
   DeepWell_96_Well,
   DiTi_100ul_Te_MO,
@@ -22,49 +23,49 @@ from pylabrobot.resources import (
 
 
 class EVOTests(unittest.IsolatedAsyncioTestCase):
-  async def asyncSetUp(self) -> None:
+  """ Test that the EVO backend is calling `send_command` correctly. """
+
+  def setUp(self) -> None:
+    super().setUp()
+
+    # mock the EVO
     self.evo = EVO(diti_count=8)
-    self.evo.send_command = unittest.mock.AsyncMock()
-    async def send_command(module, command, params=None):
+    self.evo.send_command = unittest.mock.AsyncMock() # type: ignore[method-assign]
+    async def send_command(module, command, params=None): # pylint: disable=unused-argument
       if command == "RPX": # report_x_param
-        return {"data": [2000]} # dummy value
+        return {"data": [9000]} # park position roma
       if command == "RPY": # report_y_param
-        return {"data": [2000]} # dummy value
+        return {"data": [90]} # park position roma
       if command == "RPZ": # report_z_param
         return {"data": [2000]} # dummy value
       return {"data": None}
-    self.evo.send_command.side_effect = send_command
+    self.evo.send_command.side_effect = send_command # type: ignore[method-assign]
 
     self.deck = EVO150Deck()
     self.lh = LiquidHandler(backend=self.evo, deck=self.deck)
 
-    self.evo.setup = unittest.mock.AsyncMock()
+    # setup
+    self.evo.setup = unittest.mock.AsyncMock() # type: ignore[method-assign]
     # pylint: disable=protected-access
     self.evo._num_channels = 8
     self.evo._x_range = 2000 # TODO: override report_x_param
     self.evo._y_range = 2000
     self.evo._z_range = 2000
     self.evo._roma_connected = True
-
+    self.evo._liha_connected = True
     self.evo.liha = LiHa(self.evo, EVO.LIHA)
     self.evo.roma = RoMa(self.evo, EVO.ROMA)
 
-    # await self.evo.setup()
-    await super().asyncSetUp()
-
+    # deck setup
     self.tr_carrier = DiTi_SBS_3_Pos_MCA96(name="tip_rack_carrier")
     self.tr_carrier[0] = self.tr = DiTi_100ul_Te_MO(name="tip_rack")
-    self.lh.deck.assign_child_resource(self.tr_carrier, rails=10)
+    self.deck.assign_child_resource(self.tr_carrier, rails=10)
 
     self.plate_carrier = MP_3Pos_PCR(name="plate_carrier")
     self.plate_carrier[0] = self.plate = DeepWell_96_Well(name="plate")
-    self.lh.deck.assign_child_resource(self.plate_carrier, rails=16)
+    self.deck.assign_child_resource(self.plate_carrier, rails=16)
 
     self.evo.send_command.reset_mock()
-
-  async def test_setup(self):
-    pass
-    self.evo.send_command.assert_has_calls([])
 
   async def test_pick_up_tip(self):
     op = Pickup(
@@ -74,10 +75,11 @@ class EVOTests(unittest.IsolatedAsyncioTestCase):
     )
     await self.evo.pick_up_tips([op], use_channels=[0])
 
-    self.evo.send_command.assert_has_calls([
+    self.evo.send_command.assert_has_calls([ # type: ignore[attr-defined]
       call(module="C5", command="SHZ", params=[2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000]),
       call(module="C5", command="RPX", params=[0]),
-      call(module="C5", command="PAA", params=[2380, 1991, 90, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000]),
+      call(module="C5", command="PAA",
+           params=[2380, 1991, 90, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000]),
       call(module="C5", command="PVL", params=[0, None, None, None, None, None, None, None]),
       call(module="C5", command="SEP", params=[420, None, None, None, None, None, None, None]),
       call(module="C5", command="PPR", params=[30, None, None, None, None, None, None, None]),
@@ -106,7 +108,7 @@ class EVOTests(unittest.IsolatedAsyncioTestCase):
       liquids=[(None, 100)]
     )
     await self.evo.aspirate([op], use_channels=[0])
-    self.evo.send_command.assert_has_calls([
+    self.evo.send_command.assert_has_calls([ # type: ignore[attr-defined]
       call(module="C5", command="SHZ", params=[2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000]),
       call(module="C5", command="RPX", params=[0]),
       call(module="C5", command="PAA",
@@ -146,7 +148,7 @@ class EVOTests(unittest.IsolatedAsyncioTestCase):
       liquids=[(None, 100)]
     )
     await self.evo.dispense([op], use_channels=[0])
-    self.evo.send_command.assert_has_calls([
+    self.evo.send_command.assert_has_calls([ # type: ignore[attr-defined]
       call(module="C5", command="RPX", params=[0]),
       call(module="C5", command="PAA",
            params=[3829, 2051, 90, 1355, 2000, 2000, 2000, 2000, 2000, 2000, 2000]),
@@ -160,66 +162,66 @@ class EVOTests(unittest.IsolatedAsyncioTestCase):
     op = Move(
       resource=self.plate,
       destination=self.plate_carrier[0].get_absolute_location(),
-      resource_offset=None,
-      destination_offset=None,
+      resource_offset=Coordinate.zero(),
+      destination_offset=Coordinate.zero(),
       pickup_distance_from_top=13.2,
       get_direction=GripDirection.FRONT,
       put_direction=GripDirection.FRONT,
     )
     await self.evo.move_resource(op)
-    self.evo.send_command.assert_has_calls([
-      call(module='C1', command='RPZ', params=[5]),
-      call(module='C1', command='SSM', params=[1]),
-      call(module='C1', command='SFX', params=[10000, None]),
-      call(module='C1', command='SFY', params=[5000, 1500]),
-      call(module='C1', command='SFZ', params=[1300, None]),
-      call(module='C1', command='SFR', params=[5000, 1500]),
-      call(module='C1', command='RPX', params=[0]),
-      call(module='C1', command='SAA', params=[1, 5561, 2368, 1054, 900, None, 1, 0, 0]),
-      call(module='C1', command='AAC'),
-      call(module='C1', command='RPX', params=[0]),
-      call(module='C1', command='SSM', params=[0]),
-      call(module='C1', command='PAG', params=[900]),
-      call(module='C1', command='STW', params=[1, 0, 0, 0, 135, 0]),
-      call(module='C1', command='RPX', params=[0]),
-      call(module='C1', command='SAA', params=[1, 5561, 2368, 687, 900, None, 1, 0, 1]),
-      call(module='C1', command='RPX', params=[0]),
-      call(module='C1', command='SAA', params=[1, 5561, 2368, 59, 900, None, 1, 0, 0]),
-      call(module='C1', command='AAC'),
-      call(module='C1', command='RPX', params=[0]),
-      call(module='C1', command='SFY', params=[3500, 1000]),
-      call(module='C1', command='SFR', params=[2000, 600]),
-      call(module='C1', command='SGG', params=[100, 75, None]),
-      call(module='C1', command='AGR', params=[754]),
-      call(module='C1', command='STW', params=[1, 0, 0, 0, 135, 0]),
-      call(module='C1', command='STW', params=[2, 0, 0, 0, 53, 0]),
-      call(module='C1', command='STW', params=[3, 0, 0, 0, 55, 0]),
-      call(module='C1', command='STW', params=[4, 45, 0, 0, 0, 0]),
-      call(module='C1', command='RPX', params=[0]),
-      call(module='C1', command='SAA', params=[1, 5561, 2368, 59, 900, None, 1, 0, 1]),
-      call(module='C1', command='RPX', params=[0]),
-      call(module='C1', command='SAA', params=[2, 5561, 2368, 687, 900, None, 1, 0, 2]),
-      call(module='C1', command='RPX', params=[0]),
-      call(module='C1', command='SAA', params=[3, 5561, 2368, 1054, 900, None, 1, 0, 3]),
-      call(module='C1', command='RPX', params=[0]),
-      call(module='C1', command='SAA', params=[4, 5561, 2368, 1054, 900, None, 1, 0, 4]),
-      call(module='C1', command='RPX', params=[0]),
-      call(module='C1', command='SAA', params=[5, 5561, 2368, 687, 900, None, 1, 0, 3]),
-      call(module='C1', command='RPX', params=[0]),
-      call(module='C1', command='SAA', params=[6, 5561, 2368, 59, 900, None, 1, 0, 0]),
-      call(module='C1', command='AAC'),
-      call(module='C1', command='RPX', params=[0]),
-      call(module='C1', command='PAG', params=[900]),
-      call(module='C1', command='SFY', params=[5000, 1500]),
-      call(module='C1', command='SFR', params=[5000, 1500]),
-      call(module='C1', command='RPX', params=[0]),
-      call(module='C1', command='SAA', params=[1, 5561, 2368, 59, 900, None, 1, 0, 1]),
-      call(module='C1', command='RPX', params=[0]),
-      call(module='C1', command='SAA', params=[2, 5561, 2368, 687, 900, None, 1, 0, 2]),
-      call(module='C1', command='RPX', params=[0]),
-      call(module='C1', command='SAA', params=[3, 5561, 2368, 1054, 900, None, 1, 0, 0]),
-      call(module='C1', command='AAC'),
-      call(module='C1', command='RPX', params=[0]),
-      call(module='C1', command='SFY', params=[3500, 1000]),
-      call(module='C1', command='SFR', params=[2000, 600])
+    self.evo.send_command.assert_has_calls([  # type: ignore[attr-defined]
+      call(module="C1", command="RPZ", params=[5]),
+      call(module="C1", command="SSM", params=[1]),
+      call(module="C1", command="SFX", params=[10000, None]),
+      call(module="C1", command="SFY", params=[5000, 1500]),
+      call(module="C1", command="SFZ", params=[1300, None]),
+      call(module="C1", command="SFR", params=[5000, 1500]),
+      call(module="C1", command="RPX", params=[0]),
+      call(module="C1", command="SAA", params=[1, 5561, 2368, 1054, 900, None, 1, 0, 0]),
+      call(module="C1", command="AAC"),
+      call(module="C1", command="RPX", params=[0]),
+      call(module="C1", command="SSM", params=[0]),
+      call(module="C1", command="PAG", params=[900]),
+      call(module="C1", command="STW", params=[1, 0, 0, 0, 135, 0]),
+      call(module="C1", command="RPX", params=[0]),
+      call(module="C1", command="SAA", params=[1, 5561, 2368, 687, 900, None, 1, 0, 1]),
+      call(module="C1", command="RPX", params=[0]),
+      call(module="C1", command="SAA", params=[1, 5561, 2368, 59, 900, None, 1, 0, 0]),
+      call(module="C1", command="AAC"),
+      call(module="C1", command="RPX", params=[0]),
+      call(module="C1", command="SFY", params=[3500, 1000]),
+      call(module="C1", command="SFR", params=[2000, 600]),
+      call(module="C1", command="SGG", params=[100, 75, None]),
+      call(module="C1", command="AGR", params=[754]),
+      call(module="C1", command="STW", params=[1, 0, 0, 0, 135, 0]),
+      call(module="C1", command="STW", params=[2, 0, 0, 0, 53, 0]),
+      call(module="C1", command="STW", params=[3, 0, 0, 0, 55, 0]),
+      call(module="C1", command="STW", params=[4, 45, 0, 0, 0, 0]),
+      call(module="C1", command="RPX", params=[0]),
+      call(module="C1", command="SAA", params=[1, 5561, 2368, 59, 900, None, 1, 0, 1]),
+      call(module="C1", command="RPX", params=[0]),
+      call(module="C1", command="SAA", params=[2, 5561, 2368, 687, 900, None, 1, 0, 2]),
+      call(module="C1", command="RPX", params=[0]),
+      call(module="C1", command="SAA", params=[3, 5561, 2368, 1054, 900, None, 1, 0, 3]),
+      call(module="C1", command="RPX", params=[0]),
+      call(module="C1", command="SAA", params=[4, 5561, 2368, 1054, 900, None, 1, 0, 4]),
+      call(module="C1", command="RPX", params=[0]),
+      call(module="C1", command="SAA", params=[5, 5561, 2368, 687, 900, None, 1, 0, 3]),
+      call(module="C1", command="RPX", params=[0]),
+      call(module="C1", command="SAA", params=[6, 5561, 2368, 59, 900, None, 1, 0, 0]),
+      call(module="C1", command="AAC"),
+      call(module="C1", command="RPX", params=[0]),
+      call(module="C1", command="PAG", params=[900]),
+      call(module="C1", command="SFY", params=[5000, 1500]),
+      call(module="C1", command="SFR", params=[5000, 1500]),
+      call(module="C1", command="RPX", params=[0]),
+      call(module="C1", command="SAA", params=[1, 5561, 2368, 59, 900, None, 1, 0, 1]),
+      call(module="C1", command="RPX", params=[0]),
+      call(module="C1", command="SAA", params=[2, 5561, 2368, 687, 900, None, 1, 0, 2]),
+      call(module="C1", command="RPX", params=[0]),
+      call(module="C1", command="SAA", params=[3, 5561, 2368, 1054, 900, None, 1, 0, 0]),
+      call(module="C1", command="AAC"),
+      call(module="C1", command="RPX", params=[0]),
+      call(module="C1", command="SFY", params=[3500, 1000]),
+      call(module="C1", command="SFR", params=[2000, 600])
     ])
