d2fc4bc09545eb659745f0b5d61821b0e2bbbbc3
Rick Wierenga <rick_wierenga@icloud.com>
2024-03-19 11:14:57 -0400

    use decorator

--- pylabrobot/audio/audio.py
+++ pylabrobot/audio/audio.py
@@ -3,6 +3,9 @@
 Source of audio: https://simpleguics2pygame.readthedocs.io/en/latest/_static/links/snd_links.html
 """
 
+import functools
+
+
 try:
   from IPython.display import display, Audio
   USE_AUDIO = True
@@ -10,25 +13,26 @@ except ImportError:
   USE_AUDIO = False
 
 
-def can_play_audio():
-  return USE_AUDIO
+def _audio_check(func):
+  @functools.wraps(func)
+  def wrapper(*args, **kwargs):
+    if not USE_AUDIO:
+      return
+    return func(*args, **kwargs)
+  return wrapper
 
 
 # ====== 1. Identifying items on deck (e.g. through LLD or Z-drive engagement) ======
 
+@_audio_check
 def play_not_found():
-  if not can_play_audio():
-    return
-
   display(Audio(
       url="https://codeskulptor-demos.commondatastorage.googleapis.com/pang/arrow.mp3",
       autoplay=True))
 
 
+@_audio_check
 def play_got_item():
-  if not can_play_audio():
-    return
-
   display(Audio(
       url="https://codeskulptor-demos.commondatastorage.googleapis.com/descent/gotitem.mp3",
       autoplay=True))
