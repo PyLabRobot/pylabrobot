a59e9b12f5468803d492a6f821ad58b24c3ddbeb
DonSqualo <90971750+DonSqualo@users.noreply.github.com>
2024-03-27 01:53:07 -0700

    [Chatterbox] add support for iswap commands

--- pylabrobot/liquid_handling/backends/chatterbox_backend.py
+++ pylabrobot/liquid_handling/backends/chatterbox_backend.py
@@ -13,8 +13,10 @@ from pylabrobot.liquid_handling.standard import (
   AspirationPlate,
   Dispense,
   DispensePlate,
-  Move
+  Move,
+  GripDirection,
 )
+from pylabrobot.resources.coordinate import Coordinate
 
 
 class ChatterBoxBackend(LiquidHandlerBackend):
@@ -24,6 +26,11 @@ class ChatterBoxBackend(LiquidHandlerBackend):
     """ Initialize a chatter box backend. """
     super().__init__()
     self._num_channels = num_channels
+    self._iswap_parked = True
+
+  @property
+  def iswap_parked(self) -> bool:
+    return self._iswap_parked is True
 
   async def setup(self):
     await super().setup()
@@ -71,3 +78,21 @@ class ChatterBoxBackend(LiquidHandlerBackend):
 
   async def move_resource(self, move: Move, **backend_kwargs):
     print(f"Moving {move}.")
+
+  async def get_core(self, p1: int, p2: int):
+    print(f"Getting core from {p1} to {p2}")
+
+  async def iswap_pick_up_resource(self, resource: Resource, grip_direction: GripDirection, **backend_kwargs):
+    print(f"Pick up resource {resource.name} with {grip_direction}.")
+
+  async def iswap_move_picked_up_resource(self, location: Coordinate, resource: Resource, **backend_kwargs):
+    print(f"Move picked up resource {resource.name} to {location}")
+
+  async def iswap_release_picked_up_resource(self, location: Coordinate, resource: Resource, grip_direction: GripDirection, **backend_kwargs):
+    print(f"Release picked up resource {resource.name} at {location} with {grip_direction}.")
+
+  async def send_command(self, module, command, *args, **kwargs):
+    print(f"Sending command: {module}{command} with args {args} and kwargs {kwargs}.")
+
+  async def send_raw_command(self, command: str, **kwargs):
+    print(f"Sending raw command: {command}")
