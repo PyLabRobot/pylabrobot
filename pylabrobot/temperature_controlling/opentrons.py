d0c4d65e532665e0f2b35fa4a759cf153d862017
lennijusten <lenni@securebio.org>
2024-04-23 10:21:29 -0400

    add metal blocks to ot temp control module

--- pylabrobot/temperature_controlling/opentrons.py
+++ pylabrobot/temperature_controlling/opentrons.py
@@ -25,7 +25,7 @@ class OpentronsTemperatureModuleV2(TemperatureController):
       name=name,
       size_x=193.5,
       size_y=89.2,
-      size_z=84.0, # height without any aluminum block
+      size_z=84.0,  # height without any aluminum block
       backend=OpentronsTemperatureModuleBackend(opentrons_id=opentrons_id),
       category="temperature_controller",
       model="opentrons_temperature_module_v2"
@@ -33,40 +33,57 @@ class OpentronsTemperatureModuleV2(TemperatureController):
 
     b = OpentronsTemperatureModuleBackend(opentrons_id=opentrons_id)
 
-    # verified guesses
-    # TODO: make this a proper TubeRack
-    self.tube_rack = ItemizedResource(
-      name=f"{name}_tube_rack",
-      size_x=112.0,
-      size_y=73.6,
-      size_z=140.0,
+    self.well_block_96 = ItemizedResource(
+      name=f"{name}_96_well_block",
+      size_x=128.0,
+      size_y=85.5,
+      size_z=19.0,
 
-      items=create_equally_spaced(
-        Tube,
+      items = create_equally_spaced(Tube,
+      num_items_x=12,
+      num_items_y=8,
+
+      dx=9.0,
+      dy=9.0,
+      dz=14.5,
+
+      item_dx=9.0,
+      item_dy=9.0,
+      size_x=6.0,
+      size_y=6.0,
+      size_z=14.5,
+    )
+    )
+
+    self.well_block_24 = ItemizedResource(
+      name=f"{name}_24_well_block",
+      size_x=128.0,
+      size_y=85.5,
+      size_z=42.0,
+
+      items=create_equally_spaced(Tube,
         num_items_x=6,
         num_items_y=4,
-        dx=22,
-        dy=18,
-        # dx=22 - (86 - 73.6),
-        # dy=18 - (86 - 73.6),
-        dz=45.0,
+        dx=18.5,  # distance from center of one well to center of next well on x axis
+        dy=18.5,  # distance from center of one well to center of next well on y axis
+        dz=37.0,  # depth of wells from openings on down
 
-        item_dx=17.2,
-        item_dy=17.2,
+        item_dx=17.2,  # size of square around well in x
+        item_dy=17.2,  # size of square around well in y
 
-        size_x=10.0,
-        size_y=10.0,
-        size_z=40.0,
+        size_x=12.0,  # diameter of well
+        size_y=12.0,  # diameter of well
+        size_z=37.0,  # depth of well (same as dz)
       ),
 
       category="temperature_module",
-      model="opentrons_temperature_module_v2",
+      model="opentrons_temperature_module_v2"
     )
-    self.assign_child_resource(self.tube_rack, location=Coordinate(
+
+    # todo allow user to specify block
+    self.assign_child_resource(self.well_block_24, location=Coordinate(
       x=0,
       y=0,
-      # x=(127 - 112.0)/2,
-      # y=(86 - 73.6)/2,
       z=0
     ))
 
