4a18c68583e3e77287294939614847c5b222510c
Nicolás A. Méndez <naikymen@users.noreply.github.com>
2024-05-09 20:09:58 -0300

    Add "__repr__" to itemized_resource, displaying a grid of occupied/free spots
    Example output:
    
    ```
    12x8 TipRack
        1  2  3  4  5  6  7  8  9  10 11 12
    A:  -  -  -  -  -  -  -  -  -  -  -  -
    B:  -  -  -  -  -  -  -  -  -  -  -  -
    C:  -  -  -  -  -  -  -  -  -  -  -  -
    D:  -  -  -  -  -  -  -  -  -  -  -  -
    E:  -  -  -  -  -  -  -  -  -  -  -  -
    F:  -  -  -  -  -  -  -  -  -  -  -  -
    G:  -  -  -  -  -  -  -  -  -  -  -  -
    H:  -  -  -  -  -  -  -  -  -  -  -  -
    ```

--- pylabrobot/resources/itemized_resource.py
+++ pylabrobot/resources/itemized_resource.py
@@ -364,6 +364,33 @@ class ItemizedResource(Resource, Generic[T], metaclass=ABCMeta):
 
     return make_generator(indices, batch_size, repeat)
 
+  def __repr__(self) -> str:
+    # Make a title with summary information.
+    info_str = f"{self.num_items_x}x{self.num_items_y} {self.__class__.__name__}"
+
+    if self.num_items_y > len(LETTERS):
+      # TODO: This will work up to 384-well plates.
+      return info_str + " (too many rows to print)"
+
+    # Calculate the maximum number of digits required for any column index.
+    max_digits = len(str(self.num_items_x))
+
+    # Create the header row with numbers aligned to the columns.
+    # Use right-alignment specifier.
+    header_row = "    " + " ".join(f"{i+1:<{max_digits}}" for i in range(self.num_items_x))
+
+    # Create the item grid with resource absence/presence information.
+    item_grid = [
+      ["O" if self.get_item((i, j)).children else "-" for j in range(self.num_items_x)]
+      for i in range(self.num_items_y)
+    ]
+    spacer = (" " * max(1, max_digits))
+    item_grid = [LETTERS[i] + ":  " + spacer.join(row) for i, row in enumerate(item_grid)]
+    item_grid = "\n".join(item_grid)
+
+    # Build the final representation.
+    return info_str + "\n" + header_row + "\n" + item_grid
+  
   def serialize(self) -> dict:
     return {
       **super().serialize(),
