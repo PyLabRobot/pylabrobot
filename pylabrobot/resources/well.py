d4ef20ee1154d4a4f5762dae125e6a64ec300b2e
Camillo Moschner <camillo.moschner@biocam.guide>
2024-05-25 18:51:50 +0100

    formatting

--- pylabrobot/resources/well.py
+++ pylabrobot/resources/well.py
@@ -117,7 +117,7 @@ class Well(Container):
       return None
     else:
       well_bottom_coordinate = self.get_absolute_location() + self.center()
-      return well_bottom_coordinate+Coordinate(0,0,z_offset)
+      return well_bottom_coordinate + Coordinate(0,0,z_offset)
   
   def top(self, z_offset: float = 0.0):
     """ Compute the absolute coordinate of the top of a well, with the potential
@@ -128,7 +128,7 @@ class Well(Container):
       return None
     else:
       well_top_coordinate = self.get_absolute_location() + self.center() + Coordinate(0,0,self.get_size_z())
-      return well_top_coordinate+Coordinate(0,0,z_offset)
+      return well_top_coordinate + Coordinate(0,0,z_offset)
     
   def set_liquids(self, liquids: List[Tuple[Optional["Liquid"], float]]):
     """ Set the liquids in the well.
