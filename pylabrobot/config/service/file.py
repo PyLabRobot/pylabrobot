6adaa37cd85d6584857db920c8323811f0b31530
jtaylor <jonrtaylor12@gmail.com>
2024-06-05 12:41:54 -0600

    fix: support python 3.8 & 3.9 typing

--- pylabrobot/config/service/file.py
+++ pylabrobot/config/service/file.py
@@ -6,31 +6,31 @@ from pylabrobot.config.service.reader import ConfigReader
 from pylabrobot.config.service.writer import ConfigWriter
 
 
-class FileReader(ConfigReader[str]):
+class FileReader(ConfigReader[Union[str, Path]]):
   """A ConfigReader that reads from a file"""
 
   def __init__(self, format_reader: ConfigReader[IO]):
     self.format_reader = format_reader
 
-  def read(self, r: str) -> Config:
+  def read(self, r: Union[str, Path]) -> Config:
     """Read a Config object from a file."""
     with open(r, self.format_reader.open_mode) as f:
       return self.format_reader.read(f)
 
 
-class FileWriter(ConfigWriter[str]):
+class FileWriter(ConfigWriter[Union[str, Path]]):
   """A ConfigWriter that writes to a file"""
 
   def __init__(self, format_writer: ConfigWriter[IO]):
     self.format_writer = format_writer
 
-  def write(self, w: str, cfg: Config):
+  def write(self, w: Union[str, Path], cfg: Config):
     """Write a Config object to a file."""
     with open(w, self.format_writer.open_mode) as f:
       return self.format_writer.write(f, cfg)
 
 
-class MultiReader(ConfigReader[str]):
+class MultiReader(ConfigReader[Union[str, Path]]):
   """A ConfigReader that can read from multiple formats"""
 
   def __init__(self, reader_map: Dict[str, ConfigReader]):
