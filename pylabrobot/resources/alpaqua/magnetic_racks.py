e74e169093d882933ff9cae4ccb517bb1db4a8f1
Camillo Moschner <122165124+BioCam@users.noreply.github.com>
2024-06-05 20:17:10 +0100

    Creating PlateAdapter (#152)
    * Initiating plate_adapter module
    
    * new Alpaqua magnum FLX definition as PlateAdapter
    
    * Second PlateAdapter example, Hamilton_96_adapter_188182
    
    * Unravelling correct resource to resource assignment with logic
    
    * fixing linting
    
    * make site_pedestal_z non-optional
    
    * investigtae assign_child_resource Signature
    
    * fixing linting
    
    * implement Well-grid to hole-grid compatibility check
    
    * implement Rick's _compute_child_location idea
    
    * working on linting fix
    
    * minor format
    
    * add WIP comment
    
    * move TypeError to assign_child_resource
    
    * lift protection from compute_plate_location to enable access in LiquidHandler
    
    * remove grids' spacing limitations
    
    * tiny format
    
    * remove _child_resource_location
    
    * fix type and lint
    
    * remove adapter hole spacing constraint
    
    * tiny format
    
    ---------
    
    Co-authored-by: Rick Wierenga <rick_wierenga@icloud.com>

--- pylabrobot/resources/alpaqua/magnetic_racks.py
+++ pylabrobot/resources/alpaqua/magnetic_racks.py
@@ -1,45 +1,24 @@
 """ Alpaqua magnetic racks """
-# currently implemented as MFX_modules to enable simple and fast assignment of plates to them
-from typing import Optional
+# currently implemented as PlateAdapter to enable simple and fast assignment
+# of plates to them, with self-correcting location placement
 
-from pylabrobot.resources.ml_star.mfx_modules import MFXModule
-from pylabrobot.resources.coordinate import Coordinate
-from pylabrobot.resources.resource import Resource
-from pylabrobot.resources.plate import Plate
-from pylabrobot.resources.well import WellBottomType
+from pylabrobot.resources.plate_adapter import PlateAdapter
 
 
-class _Alpaqua_96_magnum_flx(MFXModule):
-  """ Subclass of MFXModule for Alpaqua 96 magnum flx rack. Override child resource assignment
-  to only accept flat bottom plates. """
-
-  def __init__(self, name: str):
-    super().__init__(
-    name=name,
-    size_x=127.0,
-    size_y=86.0,
-    size_z=26.5,
-    skirt_height=6.2,
-    child_resource_location=Coordinate(-1.0, 0, 26.5),
-    model="Alpaqua_96_magnum_flx",
-    )
-
-  def assign_child_resource(
-    self,
-    resource: Resource,
-    location: Optional[Coordinate] = None,
-    reassign: bool = True
-  ):
-    if not isinstance(resource, Plate):
-      raise ValueError("Only plates can be assigned to Alpaqua 96 magnum flx.")
-    if resource.get_well(0).bottom_type not in {WellBottomType.U, WellBottomType.V}:
-      raise ValueError("Only plates with U or V bottom can be assigned to Alpaqua 96 magnum flx.")
-    return super().assign_child_resource(resource, location, reassign)
-
-
-def Alpaqua_96_magnum_flx(name: str) -> _Alpaqua_96_magnum_flx:
+def Alpaqua_96_magnum_flx(name: str) -> PlateAdapter:
   """ Alpaqua Engineering LLC cat. no.: A000400
   Magnetic rack for 96-well plates.
   """
-
-  return _Alpaqua_96_magnum_flx(name=name)
+  return PlateAdapter(
+    name=name,
+    size_x=127.0,
+    size_y=86.0,
+    size_z=35.0,
+    dx=9.8,
+    dy=6.8,
+    dz=27.5, # TODO: correct dz once Plate definition has been completely fixed
+    adapter_hole_size_x=8.0,
+    adapter_hole_size_y=8.0,
+    site_pedestal_z=6.2,
+    model="Alpaqua_96_magnum_flx",
+    )
