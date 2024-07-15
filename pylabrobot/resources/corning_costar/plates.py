e9e30cedaedf20cc5d5215b738c104b3294354ef
Camillo Moschner <122165124+BioCam@users.noreply.github.com>
2024-07-15 10:39:17 +0100

    Fix Plate Stacking in ResourceStack (#192)
    * troubleshooting lid assignment in ResourceStack
    
    * testing ResourceStack part 2
    
    * fixing ResourceStack.get_size_z()
    
    * testing (a) empty stack on stack site, (b) empty stack on MFX DWP module, (c) & (d) same but with plates
    
    * fix linting
    
    * tiny rewrite
    
    * support get_size_z when direction != “z”
    
    * fix typing & tests
    
    * refactor & add tests
    
    ---------
    
    Co-authored-by: Rick Wierenga <rick_wierenga@icloud.com>

--- pylabrobot/resources/corning_costar/plates.py
+++ pylabrobot/resources/corning_costar/plates.py
@@ -1048,8 +1048,8 @@ def Cos_6_wellplate_16800ul_Fb_Lid(name: str) -> Lid:
     name=name,
     size_x=127.0,
     size_y=86.0,
-    size_z=2,             # measure the total z height
-    nesting_z_height=1.9, # measure overlap between lid and plate
+    size_z=7.8,
+    nesting_z_height=6.7, # measure overlap between lid and plate
     model="Cos_6_wellplate_16800ul_Fb_Lid",
   )
 
@@ -1082,7 +1082,7 @@ def Cos_6_wellplate_16800ul_Fb(name: str, with_lid: bool = True) -> Plate:
     name=name,
     size_x=127.0,
     size_y=86.0,
-    size_z=19.85,
+    size_z=20.0,
     lid=Cos_6_wellplate_16800ul_Fb_Lid(name=name + "_lid") if with_lid else None,
     model="Cos_6_wellplate_16800ul_Fb",
     items=create_equally_spaced_2d(Well,
