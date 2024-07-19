055ee419a9a59067ddad66cc9c05312742cd5e9b
Rick Wierenga <rick_wierenga@icloud.com>
2024-07-19 23:33:07 +0200

    support reverse ranges, grid instead of diagonal (as before)

--- pylabrobot/utils/positions_tests.py
+++ pylabrobot/utils/positions_tests.py
@@ -15,4 +15,10 @@ class TestPositions(unittest.TestCase):
   def test_expand_string_range(self):
     self.assertEqual(expand_string_range("A1:A3"), ["A1", "A2", "A3"])
     self.assertEqual(expand_string_range("A1:C1"), ["A1", "B1", "C1"])
-    self.assertEqual(expand_string_range("A1:C3"), ["A1", "B2", "C3"])
+    self.assertEqual(expand_string_range("A1:C3"), ["A1", "A2", "A3",
+                                                    "B1", "B2", "B3",
+                                                    "C1", "C2", "C3"])
+
+  def test_expand_string_range_reverse(self):
+    self.assertEqual(expand_string_range("C3:C1"), ["C3", "C2", "C1"])
+    self.assertEqual(expand_string_range("C1:A1"), ["C1", "B1", "A1"])
