1a99c490dff66d744226b72a1d661dfe2291ab3e
Rick Wierenga <rick_wierenga@icloud.com>
2024-08-01 18:06:19 +0200

    remove load_from_lay_file

--- pylabrobot/resources/hamilton/hamilton_deck_tests.py
+++ pylabrobot/resources/hamilton/hamilton_deck_tests.py
@@ -10,68 +10,6 @@ from pylabrobot.resources.ml_star import STF_L, HTF_L, TIP_CAR_480_A00, PLT_CAR_
 class HamiltonDeckTests(unittest.TestCase):
   """ Tests for the HamiltonDeck class. """
 
-  # def test_parse_lay_file(self):
-  #   fn = "./pylabrobot/testing/test_data/test_deck.lay"
-  #   deck = HamiltonSTARDeck.load_from_lay_file(fn)
-
-  #   tip_car = deck.get_resource("TIP_CAR_480_A00_0001")
-  #   assert isinstance(tip_car, TipCarrier)
-
-  #   def get_item_center(name: str) -> Coordinate:
-  #     tip_rack = cast(ItemizedResource, deck.get_resource(name))
-  #     tip = cast(Resource, tip_rack.get_item("A1"))
-  #     return tip.get_absolute_location() + tip.center()
-
-  #   self.assertEqual(tip_car.get_absolute_location(), Coordinate(122.500, 63.000, 100.000))
-  #   self.assertEqual(get_item_center("tips_01"),  Coordinate(140.400, 145.800, 164.450))
-  #   self.assertEqual(get_item_center("STF_L_0001"), Coordinate(140.400, 241.800, 164.450))
-  #   self.assertEqual(get_item_center("tips_04"), Coordinate(140.400, 433.800, 131.450))
-
-  #   assert tip_car[0].resource is not None
-  #   self.assertEqual(tip_car[0].resource.name, "tips_01")
-  #   assert tip_car[1].resource is not None
-  #   self.assertEqual(tip_car[1].resource.name, "STF_L_0001")
-  #   self.assertIsNone(tip_car[2].resource)
-  #   assert tip_car[3].resource is not None
-  #   self.assertEqual(tip_car[3].resource.name, "tips_04")
-  #   self.assertIsNone(tip_car[4].resource)
-
-  #   self.assertEqual(deck.get_resource("PLT_CAR_L5AC_A00_0001").get_absolute_location(),
-  #     Coordinate(302.500, 63.000, 100.000))
-  #   self.assertEqual(get_item_center("Cos_96_DW_1mL_0001"), Coordinate(320.500, 146.000, 187.150))
-  # self.assertEqual(get_item_center("Cos_96_DW_500ul_0001"), Coordinate(320.500, 338.000, 188.150))
-  #   self.assertEqual(get_item_center("Cos_96_DW_1mL_0002"), Coordinate(320.500, 434.000, 187.150))
-  #   self.assertEqual(get_item_center("Cos_96_DW_2mL_0001"), Coordinate(320.500, 530.000, 187.150))
-
-  #   plt_car1 = deck.get_resource("PLT_CAR_L5AC_A00_0001")
-  #   assert isinstance(plt_car1, PlateCarrier)
-  #   assert plt_car1[0].resource is not None
-  #   self.assertEqual(plt_car1[0].resource.name, "Cos_96_DW_1mL_0001")
-  #   self.assertIsNone(plt_car1[1].resource)
-  #   assert plt_car1[2].resource is not None
-  #   self.assertEqual(plt_car1[2].resource.name, "Cos_96_DW_500ul_0001")
-  #   assert plt_car1[3].resource is not None
-  #   self.assertEqual(plt_car1[3].resource.name, "Cos_96_DW_1mL_0002")
-  #   assert plt_car1[4].resource is not None
-  #   self.assertEqual(plt_car1[4].resource.name, "Cos_96_DW_2mL_0001")
-
-  #   self.assertEqual(deck.get_resource("PLT_CAR_L5AC_A00_0002").get_absolute_location(),
-  #     Coordinate(482.500, 63.000, 100.000))
-  #   self.assertEqual(get_item_center("Cos_96_DW_1mL_0003"), Coordinate(500.500, 146.000, 187.150))
-  # self.assertEqual(get_item_center("Cos_96_DW_500ul_0003"), Coordinate(500.500, 242.000, 188.150))
-  #   self.assertEqual(get_item_center("Cos_96_PCR_0001"), Coordinate(500.500, 434.000, 186.650))
-
-  #   plt_car2 = deck.get_resource("PLT_CAR_L5AC_A00_0002")
-  #   assert isinstance(plt_car2, PlateCarrier)
-  #   assert plt_car2[0].resource is not None
-  #   self.assertEqual(plt_car2[0].resource.name, "Cos_96_DW_1mL_0003")
-  #   assert plt_car2[1].resource is not None
-  #   self.assertEqual(plt_car2[1].resource.name, "Cos_96_DW_500ul_0003")
-  #   self.assertIsNone(plt_car2[2].resource)
-  #   assert plt_car2[3].resource is not None
-  #   self.assertEqual(plt_car2[3].resource.name, "Cos_96_PCR_0001")
-  #   self.assertIsNone(plt_car2[4].resource)
-
   def build_layout(self):
     """ Build a deck layout for testing """
     deck = STARLetDeck()
