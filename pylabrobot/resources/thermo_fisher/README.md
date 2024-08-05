e27bdc3091a807581dde42c7e410caed08c1844e
Camillo Moschner <camillo.moschner@biocam.guide>
2024-08-05 16:42:32 +0100

    add Thermo_AB_96_wellplate_300ul_Vb_EnduraPlate base definition for testing

--- pylabrobot/resources/thermo_fisher/README.md
+++ pylabrobot/resources/thermo_fisher/README.md
@@ -23,15 +23,15 @@ Thermo Fisher Scientific Inc. (TFS, aka "Thermo")
 │   └── Pierce
 ├── Unity Lab Services (brand, services)
 ├── Patheon (brand, services)
-└── Ppd (brand, services)
+└── PPD (brand, services)
 ```
 
 ## Plates
 
 | Description               | Image              | PLR definition |
 |--------------------|--------------------|--------------------|
-| 'ThermoScientific_96_DWP_1200ul_Rd'<br>Part no.: AB-1127 or 10243223<br>[manufacturer website](https://www.fishersci.co.uk/shop/products/product/10243223) <br><br>- Material: Polypropylene (AB-1068, polystyrene) <br> | <img src="imgs/ThermoScientific_96_DWP_1200ul_Rd.webp" alt="ThermoScientific_96_DWP_1200ul_Rd" style="width:250px;"/> | `ThermoScientific_96_DWP_1200ul_Rd` |
-| 'Thermo_AB_96_wellplate_300ul_Vb_EnduraPlate'<br>Part no.: 4483354 (TFS) or 15273005 (TS) (= with barcode)<br>Part no.: 16698853 (TS) (= **without** barcode)<br>[manufacturer website](https://www.thermofisher.com/order/catalog/product/4483354) <br><br>- Material: Polycarbonate, Polypropylene<br>- plate_type: semi-skirted<br>- product line: "MicroAmp"<br>- (sub)product line: "EnduraPlate" | <img src="imgs/Thermo_AB_96_wellplate_300ul_Vb_EnduraPlate.png" alt="Thermo_AB_96_wellplate_300ul_Vb_EnduraPlate" style="width:250px;"/> | `Thermo_AB_96_wellplate_300ul_Vb_EnduraPlate` |
+| 'ThermoScientific_96_wellplate_1200ul_Rd'<br>Part no.: AB-1127 or 10243223<br>[manufacturer website](https://www.fishersci.co.uk/shop/products/product/10243223) <br><br>- Material: Polypropylene (AB-1068, polystyrene) <br> | <img src="imgs/ThermoScientific_96_wellplate_1200ul_Rd.webp" alt="ThermoScientific_96_wellplate_1200ul_Rd" style="width:250px;"/> | `ThermoScientific_96_wellplate_1200ul_Rd` |
+| 'Thermo_AB_96_wellplate_300ul_Vb_EnduraPlate'<br>Part no.: 4483354 (TFS) or 15273005 (FS) (= with barcode)<br>Part no.: 16698853 (FS) (= **without** barcode)<br>[manufacturer website](https://www.thermofisher.com/order/catalog/product/4483354) <br><br>- Material: Polycarbonate, Polypropylene<br>- plate_type: semi-skirted<br>- product line: "MicroAmp"<br>- (sub)product line: "EnduraPlate" | <img src="imgs/Thermo_AB_96_wellplate_300ul_Vb_EnduraPlate.png" alt="Thermo_AB_96_wellplate_300ul_Vb_EnduraPlate" style="width:250px;"/> | `Thermo_AB_96_wellplate_300ul_Vb_EnduraPlate` |
 
 ## Troughs
 
--- pylabrobot/resources/thermo_fisher/plates.py
+++ pylabrobot/resources/thermo_fisher/plates.py
@@ -8,7 +8,10 @@ from pylabrobot.resources.plate import Lid, Plate
 
 from pylabrobot.resources.height_volume_functions import (
   calculate_liquid_height_in_container_2segments_square_ubottom,
-  calculate_liquid_volume_container_2segments_square_ubottom)
+  calculate_liquid_volume_container_2segments_square_ubottom,
+  calculate_liquid_height_in_container_2segments_round_vbottom,
+  calculate_liquid_volume_container_2segments_round_vbottom
+  )
 
 
 # # # # # # # # # # ThermoScientific_96_wellplate_1200ul_Rd # # # # # # # # # #
@@ -100,3 +103,90 @@ def ThermoScientific_96_wellplate_1200ul_Rd_L(name: str, with_lid: bool = False)
 
 def ThermoScientific_96_wellplate_1200ul_Rd_P(name: str, with_lid: bool = False) -> Plate:
   return ThermoScientific_96_wellplate_1200ul_Rd(name=name, with_lid=with_lid).rotated(90)
+
+
+
+# # # # # # # # # # Thermo_AB_96_wellplate_300ul_Vb_EnduraPlate # # # # # # # # # #
+
+def _compute_volume_from_height_Thermo_AB_96_wellplate_300ul_Vb_EnduraPlate(h: float):
+  if h > 21.1:
+    raise ValueError(f"Height {h} is too large for" + \
+                     "ThermoScientific_96_wellplate_1200ul_Rd")
+  return calculate_liquid_volume_container_2segments_round_vbottom(
+    d=5.5,
+    h_cone=13.6,
+    h_cylinder=7.5,
+    liquid_height=h)
+
+
+def _compute_height_from_volume_Thermo_AB_96_wellplate_300ul_Vb_EnduraPlate(liquid_volume: float):
+  if liquid_volume > 315: # 5% tolerance
+    raise ValueError(f"Volume {liquid_volume} is too large for" + \
+                     "ThermoScientific_96_wellplate_1200ul_Rd")
+  return round(calculate_liquid_height_in_container_2segments_round_vbottom(
+    d=5.5,
+    h_cone=13.6,
+    h_cylinder=7.5,
+    liquid_volume=liquid_volume),3)
+
+
+def Thermo_AB_96_wellplate_300ul_Vb_EnduraPlate_Lid(name: str) -> Lid:
+  raise NotImplementedError("This lid is not currently defined.")
+  # See https://github.com/PyLabRobot/pylabrobot/pull/161.
+  # return Lid(
+  #   name=name,
+  #   size_x=127.76,
+  #   size_y=85.48,
+  #   size_z=5,
+  #   nesting_z_height=None, # measure overlap between lid and plate
+  #   model="Thermo_AB_96_wellplate_300ul_Vb_EnduraPlate_Lid",
+  # )
+
+def Thermo_AB_96_wellplate_300ul_Vb_EnduraPlate(name: str, with_lid: bool = False) -> Plate:
+  """ Thermo Fisher Scientific/Fisher Scientific cat. no.: 4483354/15273005 (= with barcode)
+  - Part no.: 16698853 (FS) (= **without** barcode).
+  - Material: Polycarbonate, Polypropylene.
+  - Sterilization compatibility: ?
+  - Chemical resistance: ?
+  - Thermal resistance: ?
+  - Cleanliness: 'Certified DNA-, RNAse-, and PCR inhibitor-free with in-process sampling tests'.
+  - ANSI/SLAS-format for compatibility with automated systems.
+  - total_volume = 300 ul.
+  - working_volume = 200 ul (recommended by manufacturer).
+  """
+  return Plate(
+    name=name,
+    size_x=127.76,
+    size_y=85.48,
+    size_z=20.1+1.0 - 0.5,
+    lid=Thermo_AB_96_wellplate_300ul_Vb_EnduraPlate_Lid(name + "_lid") if with_lid else None,
+    model="Thermo_AB_96_wellplate_300ul_Vb_EnduraPlate",
+    plate_type="semi-skirted",
+    ordered_items=create_ordered_items_2d(Well,
+      num_items_x=12,
+      num_items_y=8,
+      dx=11.63,
+      dy=9.95,
+      dz=0.0, # check that plate is semi-skirted
+      item_dx=9,
+      item_dy=9,
+      size_x=5.49,
+      size_y=5.49,
+      size_z=20.5,
+      bottom_type=WellBottomType.V,
+      material_z_thickness=1.0,
+      cross_section_type=CrossSectionType.CIRCLE,
+      compute_volume_from_height=(
+        _compute_volume_from_height_Thermo_AB_96_wellplate_300ul_Vb_EnduraPlate
+      ),
+      compute_height_from_volume=(
+        _compute_height_from_volume_Thermo_AB_96_wellplate_300ul_Vb_EnduraPlate
+      )
+    ),
+  )
+
+def Thermo_AB_96_wellplate_300ul_Vb_EnduraPlate_L(name: str, with_lid: bool = False) -> Plate:
+  return Thermo_AB_96_wellplate_300ul_Vb_EnduraPlate(name=name, with_lid=with_lid)
+
+def Thermo_AB_96_wellplate_300ul_Vb_EnduraPlate_P(name: str, with_lid: bool = False) -> Plate:
+  return Thermo_AB_96_wellplate_300ul_Vb_EnduraPlate(name=name, with_lid=with_lid).rotated(90)
