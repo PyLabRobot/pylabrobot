9ca53d8593c75576c6dfdfb3fa016f66b4cdcc43
Rick Wierenga <rick_wierenga@icloud.com>
2024-08-17 16:41:36 +0200

    indentation

--- pylabrobot/tests/serializer_tests.py
+++ pylabrobot/tests/serializer_tests.py
@@ -2,41 +2,41 @@ from pylabrobot.serializer import serialize, deserialize
 
 
 def test_serialize_deserialize_closure():
-    def outer(x):
-        y = 10
-        def inner():
-            return x + y
+  def outer(x):
+    y = 10
+    def inner():
+      return x + y
 
-        return inner
+    return inner
 
-    closure = outer(5)
-    serialized = serialize(closure)
-    deserialized = deserialize(serialized, allow_marshal=True)
+  closure = outer(5)
+  serialized = serialize(closure)
+  deserialized = deserialize(serialized, allow_marshal=True)
 
-    assert closure() == deserialized()
+  assert closure() == deserialized()
 
 
 def test_serialize_deserialize_cell():
-    x = 42
+  x = 42
 
-    def func():
-        return x
+  def func():
+    return x
 
-    assert func.__closure__ is not None
-    cell = func.__closure__[0]
-    serialized = serialize(cell)
-    deserialized = deserialize(serialized)
+  assert func.__closure__ is not None
+  cell = func.__closure__[0]
+  serialized = serialize(cell)
+  deserialized = deserialize(serialized)
 
-    assert cell.cell_contents == deserialized.cell_contents
+  assert cell.cell_contents == deserialized.cell_contents
 
 
 def test_serialize_deserialize_function_with_closure():
-    x = 10
+  x = 10
 
-    def func(y):
-        return x + y
+  def func(y):
+    return x + y
 
-    serialized = serialize(func)
-    deserialized = deserialize(serialized, allow_marshal=True)
+  serialized = serialize(func)
+  deserialized = deserialize(serialized, allow_marshal=True)
 
-    assert func(5) == deserialized(5)
+  assert func(5) == deserialized(5)
