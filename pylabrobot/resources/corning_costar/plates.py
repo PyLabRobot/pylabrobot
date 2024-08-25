c7215884ff4ee051d019690e1a64c4932b9bfd22
Marielle Russo <67157875+maraxen@users.noreply.github.com>
2024-08-25 15:24:42 -0400

    Added z and z nesting heights to Costar flat bottom 360 µL plates (#226)
    Co-authored-by: Rick Wierenga <rick_wierenga@icloud.com>

--- pylabrobot/resources/corning_costar/plates.py
+++ pylabrobot/resources/corning_costar/plates.py
@@ -1193,15 +1193,12 @@ def Cos_96_wellplate_2mL_Vb_P(name: str, with_lid: bool = False) -> Plate:
 # # # # # # # # # # Cor_96_wellplate_360ul_Fb # # # # # # # # # #
 
 def Cor_96_wellplate_360ul_Fb_Lid(name: str) -> Lid:
-  raise NotImplementedError("This lid is not currently defined.")
-  # See https://github.com/PyLabRobot/pylabrobot/pull/161.
-
   return Lid(
     name=name,
     size_x=127.76,
     size_y=85.48,
-    size_z=None,             # measure the total z height
-    nesting_z_height=None,   # measure overlap between lid and plate
+    size_z=8.9,             # measure the total z height
+    nesting_z_height=7.6,   # measure overlap between lid and plate
     model="Cor_96_wellplate_360ul_Fb_Lid",
   )
 
