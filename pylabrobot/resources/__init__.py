45ab7b15eda91207a3ea7eed713943c59a84f603
Rick Wierenga <rick_wierenga@icloud.com>
2024-09-19 19:51:56 -0700

    from __init__

--- pylabrobot/resources/__init__.py
+++ pylabrobot/resources/__init__.py
@@ -39,12 +39,9 @@ from .alpaqua import *
 from .azenta import *
 from .boekel import *
 from .corning_costar import *
-from .corning_axygen import *
 from .eppendorf import *
 from .falcon import *
-from .greiner import *
 from .hamilton import *
-from .limbro import *
 from .ml_star import *
 from .opentrons import *
 from .porvair import *
