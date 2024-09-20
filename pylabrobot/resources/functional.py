78c77c03acae901f9358568a0abb27ca93613509
Rick Wierenga <rick_wierenga@icloud.com>
2024-09-20 11:35:49 -0700

    actually support cache_file_path=None

--- pylabrobot/resources/functional.py
+++ pylabrobot/resources/functional.py
@@ -11,7 +11,7 @@ async def linear_tip_spot_generator(tip_racks: List[TipRack],
   """ Tip spot generator with disk caching. Linearly iterate through all tip spots and
   raise StopIteration when all spots have been used. """
   tip_rack_idx, tip_spot_idx = 0, 0
-  if os.path.exists(cache_file_path):
+  if cache_file_path is not None and os.path.exists(cache_file_path):
     with open(cache_file_path, "r", encoding="utf-8") as f:
       data = json.load(f)
       tip_rack_idx, tip_spot_idx = data["tri"], data["tsi"]
@@ -20,8 +20,9 @@ async def linear_tip_spot_generator(tip_racks: List[TipRack],
   while tip_rack_idx < len(tip_racks):
     while tip_spot_idx < 96:
       tip_spot_idx += 1
-      with open(cache_file_path, "w", encoding="utf-8") as f:
-        json.dump({"tri": tip_rack_idx, "tsi": tip_spot_idx}, f)
+      if cache_file_path is not None:
+        with open(cache_file_path, "w", encoding="utf-8") as f:
+          json.dump({"tri": tip_rack_idx, "tsi": tip_spot_idx}, f)
       yield tip_racks[tip_rack_idx][tip_spot_idx]
 
     tip_rack_idx += 1
@@ -36,7 +37,7 @@ async def randomized_tip_spot_generator(tip_racks: List[TipRack], K: int,
   all_tip_spots = [rack.get_item(spot_idx) for rack in tip_racks for spot_idx in range(rack.num_items)]
   recently_sampled = deque(maxlen=K)
 
-  if os.path.exists(cache_file_path):
+  if cache_file_path is not None and os.path.exists(cache_file_path):
     with open(cache_file_path, "r", encoding="utf-8") as f:
       data = json.load(f)
       recently_sampled = deque(data["recently_sampled"], maxlen=K)
@@ -51,7 +52,8 @@ async def randomized_tip_spot_generator(tip_racks: List[TipRack], K: int,
     chosen_tip_spot = random.choice(available_tips)
     recently_sampled.append(chosen_tip_spot.name)
 
-    with open(cache_file_path, "w", encoding="utf-8") as f:
-      json.dump({"recently_sampled": list(recently_sampled)}, f)
+    if cache_file_path is not None:
+      with open(cache_file_path, "w", encoding="utf-8") as f:
+        json.dump({"recently_sampled": list(recently_sampled)}, f)
 
     yield chosen_tip_spot
