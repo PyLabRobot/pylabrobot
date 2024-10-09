fb9ad273af361d529423633dcfd77c2b4f931d8f
Rick Wierenga <rick_wierenga@icloud.com>
2024-10-09 14:04:55 -0700

    also do notebooks

--- docs/combine.sh
+++ docs/combine.sh
@@ -8,7 +8,13 @@ fi
 find "$SOURCE_DIR" -name "*.md.txt" | while read file; do
   echo "Processing $file"
   cat "$file" >> "$OUTPUT_FILE"
-  echo -e "\n\n" >> "$OUTPUT_FILE"
+  echo "\n\n" >> "$OUTPUT_FILE"
+done
+
+find "$SOURCE_DIR" -name "*.ipynb.txt" | while read file; do
+  echo "Processing notebook $file"
+  jupyter nbconvert --to markdown "$file" --stdout >> "$OUTPUT_FILE"
+  echo "\n\n" >> "$OUTPUT_FILE"
 done
 
 echo "All files concatenated into $OUTPUT_FILE"
