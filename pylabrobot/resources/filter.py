82b1421845df004ed413e9896f1b9f18ad3fc82f
Ben <47133192+ben-ray@users.noreply.github.com>
2025-01-03 23:00:24 -0800

    fix positioning logic for move cmds

--- pylabrobot/resources/filter.py
+++ pylabrobot/resources/filter.py
@@ -1,18 +1,9 @@
-from typing import Optional, Union
-
-from pylabrobot.liquid_handling.liquid_handler import LiquidHandler
-from pylabrobot.resources.carrier import ResourceHolder
-from pylabrobot.resources.coordinate import Coordinate
-from pylabrobot.resources.plate import Plate
+from typing import Optional
 from pylabrobot.resources.resource import Resource
 
 
 class Filter(Resource):
   """Filter for plates for use in filtering cells before flow cytometry."""
-
-  filter_dispense_offset = Coordinate(
-    0, 0, 7
-  )  # height to pipette through filter (required pressure on filter)
 
   def __init__(
     self,
@@ -24,58 +15,5 @@ class Filter(Resource):
     model: Optional[str] = None,
   ):
     super().__init__(
-      name=name,
-      size_x=size_x,
-      size_y=size_y,
-      size_z=size_z,
-      category=category,
-      model=model,
+      name=name, size_x=size_x, size_y=size_y, size_z=size_z, category=category, model=model
     )
-
-  async def move_filter(
-    self,
-    lh: LiquidHandler,
-    to_dest: Union[Plate, ResourceHolder, Coordinate],
-    arm: str = "core",
-    **kwargs,
-  ):
-    """move filter from CarrierSite to a Plate using core grippers (faster) or iSWAP (slower)"""
-    await lh.move_resource(
-      resource=self,
-      to=to_dest,
-      use_arm=arm,
-      pickup_distance_from_top=15,
-      core_grip_strength=20,
-      return_core_gripper=True,
-      **kwargs,
-    )
-
-  async def dispense_through_filter(
-    self, indices: list[int], volume: float, lh: LiquidHandler, **disp_kwargs
-  ):
-    assert isinstance(self.parent, Plate), "Filter must be placed on a plate to be pipetted."
-
-    offsets = disp_kwargs.get("offsets", self.filter_dispense_offset)
-    if not isinstance(offsets, Coordinate):
-      raise ValueError("Offsets must be a Coordinate.")
-
-    defaults = {
-      "offsets": [offsets + self.filter_dispense_offset] * len(indices)
-      if isinstance(offsets, Coordinate)
-      else [offsets] * len(indices),
-      "transport_air_volume": 5,
-      "swap_speed": 100,
-      "minimum_traverse_height_at_beginning_of_a_command": self.parent.get_absolute_location(
-        "c", "c", "t"
-      ).z
-      + 20,
-      "min_z_endpos": self.parent.get_absolute_location("c", "c", "t").z + 20,
-    }
-
-    disp_params = {**defaults, **{k: v for k, v in disp_kwargs.items() if k in defaults}}
-
-    await lh.dispense([self.parent[i][0] for i in indices], [volume] * len(indices), **disp_params)
-
-
-def RetroFilterv4(name: str) -> Filter:
-  return Filter(name=name, size_x=129, size_y=88, size_z=19.7)
---
+++ pylabrobot/resources/retro_filter/retro_filter.py
@@ -0,0 +1,73 @@
+from typing import Optional
+from pylabrobot.liquid_handling.liquid_handler import LiquidHandler
+from pylabrobot.resources.coordinate import Coordinate
+from pylabrobot.resources.filter import Filter
+from pylabrobot.resources.plate import Plate
+
+
+class RetroFilter(Filter):
+  filter_dispense_offset = Coordinate(
+    0, 0, 7
+  )  # height to pipette through filter (required pressure on filter)
+
+  def __init__(
+    self,
+    name: str,
+    size_x: float = 129,
+    size_y: float = 88,
+    size_z: float = 19.7,
+    category: str = "filter",
+    model: Optional[str] = None,
+    nesting_z_height: float = 15,
+  ):
+    self.nesting_z_height = nesting_z_height
+    super().__init__(
+      name=name, size_x=size_x, size_y=size_y, size_z=size_z, category=category, model=model
+    )
+
+  async def move_filter(
+    self,
+    lh: LiquidHandler,
+    to_dest: Plate,  # lh drop_resource only supports filters on Plates (for now)
+    arm: str = "core",
+    channel_1=1,
+    channel_2=2,
+    **kwargs,
+  ):
+    """move filter from CarrierSite to a Plate using core grippers (faster) or iSWAP (slower)"""
+
+    pickup_kwargs = kwargs.copy()
+    if arm == "core":
+      pickup_kwargs.update(
+        {"core_grip_strength": 20, "channel_1": channel_1, "channel_2": channel_2}
+      )
+    await lh.pick_up_resource(
+      resource=self, use_arm=arm, pickup_distance_from_top=15, **pickup_kwargs
+    )
+
+    await lh.drop_resource(destination=to_dest, **kwargs)
+
+  async def dispense_through_filter(
+    self, indices: list[int], volume: float, lh: LiquidHandler, **disp_kwargs
+  ):
+    assert isinstance(self.parent, Plate), "Filter must be placed on a plate to be pipetted."
+
+    offsets = disp_kwargs.get("offsets", self.filter_dispense_offset)
+    if not isinstance(offsets, Coordinate):
+      raise ValueError("Offsets must be a Coordinate.")
+
+    defaults = {
+      "offsets": [offsets + self.filter_dispense_offset] * len(indices)
+      if isinstance(offsets, Coordinate)
+      else [offsets] * len(indices),
+      "transport_air_volume": 5,
+      "swap_speed": 100,
+      "minimum_traverse_height_at_beginning_of_a_command": self.parent.get_absolute_location(
+        "c", "c", "t"
+      ).z
+      + 20,
+      "min_z_endpos": self.parent.get_absolute_location("c", "c", "t").z + 20,
+    }
+
+    disp_params = {**defaults, **{k: v for k, v in disp_kwargs.items() if k in defaults}}
+    await lh.dispense([self.parent[i][0] for i in indices], [volume] * len(indices), **disp_params)
