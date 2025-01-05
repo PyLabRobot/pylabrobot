0d99c234f962f3a9b180ef28b3b3353020695a6b
Camillo Moschner <camillo.moschner@biocam.guide>
2025-01-05 14:31:16 +0000

    `make format`

--- pylabrobot/resources/hamilton/__init__.py
+++ pylabrobot/resources/hamilton/__init__.py
@@ -1,21 +1,14 @@
-# Machine information
 from .hamilton_decks import (
     HamiltonDeck,
     HamiltonSTARDeck,
     STARDeck,
     STARLetDeck,
 )
-
-# Carrier information
 from .mfx_carriers import *
-
-# Misc information
 from .mfx_modules import *
 from .plate_adapters import *
 from .plate_carriers import *
 from .tip_carriers import *
-
-# Tip information
 from .tip_creators import *
 from .tip_racks import *
 from .trough_carriers import *
