7960297685879a285773226f3fb37d5193a961d3
Rick Wierenga <rick_wierenga@icloud.com>
2025-03-11 12:34:38 -0700

    dont need lh in evo tests

--- pylabrobot/liquid_handling/backends/tecan/EVO_tests.py
+++ pylabrobot/liquid_handling/backends/tecan/EVO_tests.py
@@ -2,7 +2,6 @@ import unittest
 import unittest.mock
 from unittest.mock import call
 
-from pylabrobot.liquid_handling import LiquidHandler
 from pylabrobot.liquid_handling.backends.tecan.EVO import (
   EVO,
   LiHa,
@@ -49,7 +48,6 @@ class EVOTests(unittest.IsolatedAsyncioTestCase):
     self.evo.send_command.side_effect = send_command  # type: ignore[method-assign]
 
     self.deck = EVO150Deck()
-    self.lh = LiquidHandler(backend=self.evo, deck=self.deck)
 
     # setup
     self.evo.setup = unittest.mock.AsyncMock()  # type: ignore[method-assign]
