0c10313a630da3eb2da677026d72be952c081ddd
Rick Wierenga <rick_wierenga@icloud.com>
2025-03-17 15:58:16 -0700

    add HHS resource model

--- pylabrobot/heating_shaking/__init__.py
+++ pylabrobot/heating_shaking/__init__.py
@@ -1,5 +1,7 @@
 """A hybrid between pylabrobot.heating and pylabrobot.temperature_controlling"""
 
 from pylabrobot.heating_shaking.backend import HeaterShakerBackend
+from pylabrobot.heating_shaking.hamilton import hamilton_heater_shaker
+from pylabrobot.heating_shaking.hamilton_backend import HamiltonHeatShakerBackend
 from pylabrobot.heating_shaking.heater_shaker import HeaterShaker
 from pylabrobot.heating_shaking.inheco import InhecoThermoShake
