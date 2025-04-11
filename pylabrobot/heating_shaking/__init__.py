77b1d654fb66cb73abdd8d2047bccfb882692402
Rick Wierenga <rick_wierenga@icloud.com>
2025-04-11 13:44:41 -0700

    Fix serialization (#457)

--- pylabrobot/heating_shaking/__init__.py
+++ pylabrobot/heating_shaking/__init__.py
@@ -1,5 +1,7 @@
-"""A hybrid between pylabrobot.heating and pylabrobot.temperature_controlling"""
+"""A hybrid between pylabrobot.shaking and pylabrobot.temperature_controlling"""
 
 from pylabrobot.heating_shaking.backend import HeaterShakerBackend
+from pylabrobot.heating_shaking.chatterbox import HeaterShakerChatterboxBackend
+from pylabrobot.heating_shaking.hamilton import HamiltonHeatShaker
 from pylabrobot.heating_shaking.heater_shaker import HeaterShaker
 from pylabrobot.heating_shaking.inheco import InhecoThermoShake
