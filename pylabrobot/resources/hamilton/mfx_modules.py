d42f6f1eca58e7e5c829a7864001f45439398e60
Edison Lin <linedison3@gmail.com>
2025-04-17 10:46:32 -0400

    update Hamilton_MFXmodule_tall_flat docstring and adjust size_x parameter

--- pylabrobot/resources/hamilton/mfx_modules.py
+++ pylabrobot/resources/hamilton/mfx_modules.py
@@ -64,10 +64,8 @@ def MFX_DWP_module_flat(name: str) -> PlateHolder:
 
 
 def Hamilton_MFXmodule_tall_flat(name: str) -> PlateHolder:
-  """Hamilton part. no.: 92585-01 (RNO) (Uncertain)
+  """Hamilton cat. no.: 188228 (BSZ)
   Module to position a Well Plate.
-
-  Also could be Hamilton cat.no.: 188228
   """
 
   # Resource size is measured with caliper.
@@ -75,7 +73,7 @@ def Hamilton_MFXmodule_tall_flat(name: str) -> PlateHolder:
 
   return PlateHolder(
     name=name,
-    size_x=134.0,
+    size_x=135.0,
     size_y=94.0,
     size_z=216 - 18.195 - 100,  # mesured using probe
     # probe height - carrier_height - deck_height
