56b08c2f442fcdd1c36be10fd9d485e8456bb15c
Rick Wierenga <rick_wierenga@icloud.com>
2025-05-07 15:20:48 -0700

    consider pickup_distance_from_top in STARBackend. move_picked_up_resource (#490)

--- pylabrobot/liquid_handling/liquid_handler.py
+++ pylabrobot/liquid_handling/liquid_handler.py
@@ -1832,6 +1832,7 @@ class LiquidHandler(Resource, Machine):
         location=to,
         resource=self._resource_pickup.resource,
         gripped_direction=self._resource_pickup.direction,
+        pickup_distance_from_top=self._resource_pickup.pickup_distance_from_top,
       ),
       **backend_kwargs,
     )
