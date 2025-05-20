abd343dc79b675d037c1b54ebc64a81f0fba67da
Rick Wierenga <rick_wierenga@icloud.com>
2025-05-20 11:49:17 -0700

    lint

--- pylabrobot/liquid_handling/liquid_handler.py
+++ pylabrobot/liquid_handling/liquid_handler.py
@@ -1834,7 +1834,7 @@ class LiquidHandler(Resource, Machine):
         resource=self._resource_pickup.resource,
         gripped_direction=self._resource_pickup.direction,
         pickup_distance_from_top=self._resource_pickup.pickup_distance_from_top,
-        offset=offset
+        offset=offset,
       ),
       **backend_kwargs,
     )
