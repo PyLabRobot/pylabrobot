00fe96654244532a036e6b99edb729e051d98777
Harley King <harleyk@gmail.com>
2025-07-19 15:37:15 -0400

    commented meas vs spec for 188042 module

--- pylabrobot/resources/hamilton/mfx_modules.py
+++ pylabrobot/resources/hamilton/mfx_modules.py
@@ -48,22 +48,19 @@ def MFX_DWP_module_188042(name: str) -> PlateHolder:
   https://www.hamiltoncompany.com/other-robotics/188042
   """
   # ── module footprint ──────────────
-  # size_x = 132.50          # mm
-  size_x = 135
-  # size_y = 92.05           # mm
-  size_y = 94
+  size_x = 135 # measured
+  size_y = 94 # measured
 
   # ── vertical geometry ──────────────────────────────
-  pedestal_size_z = 6.45  # The height of the pedestal
-  # size_z = 178.3 - 18.195 - 100   # 60.10mm is module height above MFX carrier. This height does not including pedestal
+  pedestal_size_z = 6.45  # measured The height of the pedestal
   size_z = (
     184.745 - 18.195 - 100
   )  # 60.10mm is module height above MFX carrier. This height includes pedestal.
-  # module.get_absolute_size_y() = 184.745 puts channel right on the pedestal
+  # module.get_absolute_size_y() = 184.745 puts channel right on the pedestal with 50ul fil tip
 
   # ── where the plate’s lower-left corner sits ───────
-  child_x = 4.0  # mm
-  child_y = 3.5  # mm
+  child_x = 4.0  # mm measured
+  child_y = 3.5  # mm measured
   child_location = Coordinate(child_x, child_y, size_z)
 
   return PlateHolder(
