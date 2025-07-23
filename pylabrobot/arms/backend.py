9e90c992d4b2e82fb2d28f35b22b6f1c281cfe25
Michael Salmi <miikee713@gmail.com>
2025-07-23 19:53:57 -0400

    Adds driver for PreciseFlex PF400. Adds TCP connection for io.

---
+++ pylabrobot/arms/backend.py
@@ -0,0 +1,23 @@
+from abc import ABCMeta, abstractmethod
+
+from pylabrobot.machines.backend import MachineBackend
+
+
+class ArmBackend(MachineBackend, metaclass=ABCMeta):
+    """Backend for a robotic arm"""
+
+    @abstractmethod
+    async def move_to(self, position: tuple[float, float, float]):
+        """Move the arm to a specified position in 3D space."""
+
+    @abstractmethod
+    async def get_position(self) -> tuple[float, float, float]:
+        """Get the current position of the arm in 3D space."""
+
+    @abstractmethod
+    async def set_speed(self, speed: float):
+        """Set the speed of the arm's movement."""
+
+    @abstractmethod
+    async def get_speed(self) -> float:
+        """Get the current speed of the arm's movement."""
