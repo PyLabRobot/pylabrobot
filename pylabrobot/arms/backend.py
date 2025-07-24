bdef71661bf9d5d6109d44cd0507442e2f56daa1
Michael Salmi <miikee713@gmail.com>
2025-07-23 20:37:49 -0400

    Adds open_gripper and close_gripper methods to ArmBackend.  Add ... to the Backend abstractmethods

--- pylabrobot/arms/backend.py
+++ pylabrobot/arms/backend.py
@@ -9,15 +9,29 @@ class ArmBackend(MachineBackend, metaclass=ABCMeta):
     @abstractmethod
     async def move_to(self, position: tuple[float, float, float]):
         """Move the arm to a specified position in 3D space."""
+        ...
 
     @abstractmethod
     async def get_position(self) -> tuple[float, float, float]:
         """Get the current position of the arm in 3D space."""
+        ...
 
     @abstractmethod
     async def set_speed(self, speed: float):
         """Set the speed of the arm's movement."""
+        ...
 
     @abstractmethod
     async def get_speed(self) -> float:
         """Get the current speed of the arm's movement."""
+        ...
+
+    @abstractmethod
+    async def open_gripper(self):
+        """Open the arm's gripper."""
+        ...
+
+    @abstractmethod
+    async def close_gripper(self):
+        """Close the arm's gripper."""
+        ...
