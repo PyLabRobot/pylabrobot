cf15ba37456a20591599f7e69288f9e5641a136c
Rick Wierenga <rick_wierenga@icloud.com>
2025-07-29 16:18:00 -0700

    (de)serialize protocol

--- pylabrobot/thermocycling/standard.py
+++ pylabrobot/thermocycling/standard.py
@@ -11,6 +11,13 @@ class Step:
   hold_seconds: float
   rate: Optional[float] = None  # degrees Celsius per second
 
+  def serialize(self) -> dict:
+    return {
+      "temperature": self.temperature,
+      "hold_seconds": self.hold_seconds,
+      "rate": self.rate,
+    }
+
 
 @dataclass
 class Stage:
@@ -19,12 +26,34 @@ class Stage:
   steps: List[Step]
   repeats: int
 
+  def serialize(self) -> dict:
+    return {
+      "steps": [step.serialize() for step in self.steps],
+      "repeats": self.repeats,
+    }
+
 
 @dataclass
 class Protocol:
   """Represents a thermocycler protocol ("cycle") with multiple stages."""
 
   stages: List[Union[Stage, Step]]
+
+  def serialize(self) -> dict:
+    return {
+      "stages": [stage.serialize() for stage in self.stages],
+    }
+
+  @classmethod
+  def deserialize(cls, data: dict) -> 'Protocol':
+    stages = []
+    for stage_data in data.get("stages", []):
+      if "steps" in stage_data:
+        steps = [Step(**step) for step in stage_data["steps"]]
+        stages.append(Stage(steps=steps, repeats=stage_data.get("repeats", 1)))
+      else:
+        stages.append(Step(**stage_data))
+    return cls(stages=stages)
 
 
 class LidStatus(enum.Enum):
