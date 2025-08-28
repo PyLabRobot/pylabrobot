cecf70e9195bfaae6bd627889f1faa213e1dc822
Rick Wierenga <rick_wierenga@icloud.com>
2025-08-28 11:59:29 -0700

    async setup of STAR components

--- pylabrobot/liquid_handling/backends/hamilton/STAR_backend.py
+++ pylabrobot/liquid_handling/backends/hamilton/STAR_backend.py
@@ -1,3 +1,4 @@
+import asyncio
 import datetime
 import enum
 import functools
@@ -1381,32 +1382,39 @@ class STARBackend(HamiltonLiquidHandler, HamiltonHeaterShakerInterface):
 
       await self.pre_initialize_instrument()
 
-    if not initialized or any(tip_presences):
-      await self.initialize_pip()
+    async def pip():
+      if not initialized or any(tip_presences):
+        await self.initialize_pip()
 
-    if self.autoload_installed and not skip_autoload:
-      autoload_initialized = await self.request_autoload_initialization_status()
-      if not autoload_initialized:
-        await self.initialize_autoload()
+    async def autoload():
+      if self.autoload_installed and not skip_autoload:
+        autoload_initialized = await self.request_autoload_initialization_status()
+        if not autoload_initialized:
+          await self.initialize_autoload()
 
-      await self.park_autoload()
+        await self.park_autoload()
 
-    if self.iswap_installed and not skip_iswap:
-      iswap_initialized = await self.request_iswap_initialization_status()
-      if not iswap_initialized:
-        await self.initialize_iswap()
+    async def iswap():
+      if self.iswap_installed and not skip_iswap:
+        iswap_initialized = await self.request_iswap_initialization_status()
+        if not iswap_initialized:
+          await self.initialize_iswap()
 
-      await self.park_iswap(
-        minimum_traverse_height_at_beginning_of_a_command=int(self._iswap_traversal_height * 10)
-      )
-
-    if self.core96_head_installed and not skip_core96_head:
-      core96_head_initialized = await self.request_core_96_head_initialization_status()
-      if not core96_head_initialized:
-        await self.initialize_core_96_head(
-          trash96=self.deck.get_trash_area96(),
-          z_position_at_the_command_end=self._channel_traversal_height,
+        await self.park_iswap(
+          minimum_traverse_height_at_beginning_of_a_command=int(self._iswap_traversal_height * 10)
         )
+
+    async def core96_head():
+      if self.core96_head_installed and not skip_core96_head:
+        core96_head_initialized = await self.request_core_96_head_initialization_status()
+        if not core96_head_initialized:
+          await self.initialize_core_96_head(
+            trash96=self.deck.get_trash_area96(),
+            z_position_at_the_command_end=self._channel_traversal_height,
+          )
+
+    await pip()
+    await asyncio.gather(autoload(), iswap(), core96_head())
 
     # After setup, STAR will have thrown out anything mounted on the pipetting channels, including
     # the core grippers.
