e89b24b795ed2f6588bc6feadba9fa24c91bf19d
Camillo Moschner <camillo.moschner@biocam.guide>
2025-10-03 12:25:04 +0100

    make InhecoIncubatorShakerBackend an Incubator

--- pylabrobot/storage/inheco/__init__.py
+++ pylabrobot/storage/inheco/__init__.py
@@ -1,6 +1,6 @@
 """A hybrid between pylabrobot.shaking and pylabrobot.temperature_controlling"""
 
-from pylabrobot.incubator_shaker.backend import IncubatorShakerBackend
-from pylabrobot.incubator_shaker.inheco_incubator_shaker_backend import (
+from .backend import IncubatorShakerBackend
+from .inheco_incubator_shaker_backend import (
   InhecoIncubatorShakerBackend,
 )
