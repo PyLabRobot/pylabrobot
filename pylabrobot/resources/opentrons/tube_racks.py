316b36b612262ad0ac54c6af7eeeb8c6189060c0
Harley King <harleyk@gmail.com>
2025-10-16 16:16:56 -0400

    Accepted incoming and current to prevent conflicts

--- pylabrobot/resources/opentrons/tube_racks.py
+++ pylabrobot/resources/opentrons/tube_racks.py
@@ -1,4 +1,295 @@
-from pylabrobot.resources.opentrons.load import load_ot_tube_rack
+from typing import cast
+
+from pylabrobot.resources.opentrons.load import (
+  load_shared_opentrons_resource,
+)
+from pylabrobot.resources.tube_rack import TubeRack
+from pylabrobot.resources.tube import Tube
+from pylabrobot.resources.utils import create_ordered_items_2d
+
+
+def opentrons_24_tuberack_eppendorf_2ml_safelock_snapcap(
+  name: str,
+) -> TubeRack:
+  return cast(
+    TubeRack,
+    load_shared_opentrons_resource(
+      definition="opentrons_24_tuberack_eppendorf_2ml_safelock_snapcap",
+      name=name,
+      version=1,
+    ),
+  )
+
+
+def opentrons_24_tuberack_eppendorf_2ml_safelock_snapcap_acrylic(
+  name: str,
+) -> TubeRack:
+  return cast(
+    TubeRack,
+    load_shared_opentrons_resource(
+      definition="opentrons_24_tuberack_eppendorf_2ml_safelock_snapcap_acrylic",
+      name=name,
+      version=1,
+    ),
+  )
+
+
+def opentrons_6_tuberack_falcon_50ml_conical(name: str) -> TubeRack:
+  return cast(
+    TubeRack,
+    load_shared_opentrons_resource(
+      definition="opentrons_6_tuberack_falcon_50ml_conical",
+      name=name,
+      version=1,
+    ),
+  )
+
+
+def opentrons_15_tuberack_nest_15ml_conical(name: str) -> TubeRack:
+  return cast(
+    TubeRack,
+    load_shared_opentrons_resource(
+      definition="opentrons_15_tuberack_nest_15ml_conical",
+      name=name,
+      version=1,
+    ),
+  )
+
+
+def opentrons_24_tuberack_nest_2ml_screwcap(name: str) -> TubeRack:
+  return cast(
+    TubeRack,
+    load_shared_opentrons_resource(
+      definition="opentrons_24_tuberack_nest_2ml_screwcap",
+      name=name,
+      version=1,
+    ),
+  )
+
+
+def opentrons_24_tuberack_generic_0point75ml_snapcap_acrylic(
+  name: str,
+) -> TubeRack:
+  return cast(
+    TubeRack,
+    load_shared_opentrons_resource(
+      definition="opentrons_24_tuberack_generic_0.75ml_snapcap_acrylic",
+      name=name,
+      version=1,
+    ),
+  )
+
+
+def opentrons_10_tuberack_nest_4x50ml_6x15ml_conical(
+  name: str,
+) -> TubeRack:
+  return cast(
+    TubeRack,
+    load_shared_opentrons_resource(
+      definition="opentrons_10_tuberack_nest_4x50ml_6x15ml_conical",
+      name=name,
+      version=1,
+    ),
+  )
+
+
+def opentrons_10_tuberack_falcon_4x50ml_6x15ml_conical_acrylic(
+  name: str,
+) -> TubeRack:
+  return cast(
+    TubeRack,
+    load_shared_opentrons_resource(
+      definition="opentrons_10_tuberack_falcon_4x50ml_6x15ml_conical_acrylic",
+      name=name,
+      version=1,
+    ),
+  )
+
+
+def opentrons_24_tuberack_nest_1point5ml_screwcap(
+  name: str,
+) -> TubeRack:
+  return cast(
+    TubeRack,
+    load_shared_opentrons_resource(
+      definition="opentrons_24_tuberack_nest_1.5ml_screwcap",
+      name=name,
+      version=1,
+    ),
+  )
+
+
+def opentrons_24_tuberack_nest_1point5ml_snapcap(
+  name: str,
+) -> TubeRack:
+  return cast(
+    TubeRack,
+    load_shared_opentrons_resource(
+      definition="opentrons_24_tuberack_nest_1.5ml_snapcap",
+      name=name,
+      version=1,
+    ),
+  )
+
+
+def opentrons_10_tuberack_falcon_4x50ml_6x15ml_conical(
+  name: str,
+) -> TubeRack:
+  return cast(
+    TubeRack,
+    load_shared_opentrons_resource(
+      definition="opentrons_10_tuberack_falcon_4x50ml_6x15ml_conical",
+      name=name,
+      version=1,
+    ),
+  )
+
+
+def opentrons_24_tuberack_nest_2ml_snapcap(name: str) -> TubeRack:
+  return cast(
+    TubeRack,
+    load_shared_opentrons_resource(
+      definition="opentrons_24_tuberack_nest_2ml_snapcap",
+      name=name,
+      version=1,
+    ),
+  )
+
+
+def opentrons_24_tuberack_nest_0point5ml_screwcap(
+  name: str,
+) -> TubeRack:
+  return cast(
+    TubeRack,
+    load_shared_opentrons_resource(
+      definition="opentrons_24_tuberack_nest_0.5ml_screwcap",
+      name=name,
+      version=1,
+    ),
+  )
+
+
+def opentrons_24_tuberack_eppendorf_1point5ml_safelock_snapcap(
+  name: str,
+) -> TubeRack:
+  return cast(
+    TubeRack,
+    load_shared_opentrons_resource(
+      definition="opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap",
+      name=name,
+      version=1,
+    ),
+  )
+
+
+def opentrons_6_tuberack_nest_50ml_conical(name: str) -> TubeRack:
+  return cast(
+    TubeRack,
+    load_shared_opentrons_resource(
+      definition="opentrons_6_tuberack_nest_50ml_conical",
+      name=name,
+      version=1,
+    ),
+  )
+
+
+def opentrons_15_tuberack_falcon_15ml_conical(name: str) -> TubeRack:
+  return cast(
+    TubeRack,
+    load_shared_opentrons_resource(
+      definition="opentrons_15_tuberack_falcon_15ml_conical",
+      name=name,
+      version=1,
+    ),
+  )
+
+
+def opentrons_24_tuberack_generic_2ml_screwcap(name: str) -> TubeRack:
+  return cast(
+    TubeRack,
+    load_shared_opentrons_resource(
+      definition="opentrons_24_tuberack_generic_2ml_screwcap",
+      name=name,
+      version=1,
+    ),
+  )
+
+
+def opentrons_96_well_aluminum_block(name: str) -> TubeRack:
+  return cast(
+    TubeRack,
+    load_shared_opentrons_resource(
+      definition="opentrons_96_well_aluminum_block",
+      name=name,
+      version=1,
+    ),
+  )
+
+
+def opentrons_24_aluminumblock_generic_2ml_screwcap(
+  name: str,
+) -> TubeRack:
+  return cast(
+    TubeRack,
+    load_shared_opentrons_resource(
+      definition="opentrons_24_aluminumblock_generic_2ml_screwcap",
+      name=name,
+      version=2,
+    ),
+  )
+
+
+def opentrons_24_aluminumblock_nest_1point5ml_snapcap(
+  name: str,
+) -> TubeRack:
+  return cast(
+    TubeRack,
+    load_shared_opentrons_resource(
+      definition="opentrons_24_aluminumblock_nest_1.5ml_snapcap",
+      name=name,
+      version=1,
+    ),
+  )
+
+def opentrons_24_tuberack_generic_1point5ml_snapcap_short(name: str) -> TubeRack:
+  """
+  OpenTrons 24 well rack with the shorter stand
+  3D print available here: https://www.thingiverse.com/thing:3405002
+  Spec sheet (json):
+  https://raw.githubusercontent.com/Opentrons/opentrons/edge/shared-data/labware/definitions/2/opentrons_24_tuberack_nest_1.5ml_screwcap/1.json
+  """
+
+  INNER_WELL_WIDTH  = 9.2 # measured
+  INNER_WELL_LENGTH = 9.2 # measured
+  WELL_DEPTH        = 37.40 # measured
+  TUBE_MAX_VOL      = 1750  # µL  (generic 1.75 mL snap-cap)
+
+  tube_kwargs = {
+    "size_x": INNER_WELL_WIDTH,
+    "size_y": INNER_WELL_LENGTH,
+    "size_z": WELL_DEPTH,
+    "max_volume": TUBE_MAX_VOL,
+    "material_z_thickness": 0.80
+  }
+
+  return TubeRack(
+    name=name,
+    size_x=127.75, # spec
+    size_y=85.50, # spec
+    size_z=48.5, # measured. This is on the short 3D printed rack and not in the sheet
+    model=opentrons_24_tuberack_generic_1point5ml_snapcap_short.__name__,
+    ordered_items=create_ordered_items_2d(
+        Tube,
+        num_items_x=6,
+        num_items_y=4,
+        dx=12.5, # measured
+        dy=15.8, # measured
+        dz=12, # measured
+        item_dx=19.89, # spec
+        item_dy=19.28, # spec
+        **tube_kwargs,
+    ),
+  )from pylabrobot.resources.opentrons.load import load_ot_tube_rack
 from pylabrobot.resources.tube_rack import TubeRack
 
 
