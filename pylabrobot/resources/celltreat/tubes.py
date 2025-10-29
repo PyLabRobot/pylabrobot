6181b860b54c0c2a7f769a1e16852c6fdf229c61
ricomnl <rmeinl97@gmail.com>
2025-10-29 18:13:54 +0000

    Added tube definitions

---
+++ pylabrobot/resources/celltreat/tubes.py
@@ -0,0 +1,19 @@
+from pylabrobot.resources.tube import Tube
+
+
+def celltreat_15000ul_centrifuge_tube_Vb(name: str) -> Tube:
+  """CELLTREAT® Centrifuge Tubes-RackMaster 15mL Centrifuge Tube, Best Value - Paperboard Rack, Sterile
+  Part no.: 229414
+
+  - bottom_type=TubeBottomType.V
+  """
+  diameter = 14.7
+  return Tube(
+    name=name,
+    size_x=diameter,
+    size_y=diameter,
+    size_z=190.5,
+    model=celltreat_15000ul_centrifuge_tube_Vb.__name__,
+    max_volume=15_000,  # units: ul
+    material_z_thickness=0.8,
+  )
--- pylabrobot/resources/eppendorf/tubes.py
+++ pylabrobot/resources/eppendorf/tubes.py
@@ -29,3 +29,21 @@ def Eppendorf_Protein_LoBind_1_5ml_Vb(name: str) -> Tube:
   Same as Eppendorf_DNA_LoBind_1_5ml_Vb
   """
   return Eppendorf_DNA_LoBind_1_5ml_Vb(name=name, model="Eppendorf_Protein_LoBind_1_5ml_Vb")
+
+
+def Eppendorf_DNA_LoBind_2ml_Ub(name: str) -> Tube:
+  """2 mL round-bottom snap-cap Eppendorf tube. cat. no.: 022431048
+
+  - bottom_type=TubeBottomType.U
+  - snap-cap lid
+  """
+  diameter = 10.33
+  return Tube(
+    name=name,
+    size_x=diameter,
+    size_y=diameter,
+    size_z=41,
+    model="Eppendorf_DNA_LoBind_2ml_Ub",
+    max_volume=2000,  # units: ul
+    material_z_thickness=0.8,
+  )
