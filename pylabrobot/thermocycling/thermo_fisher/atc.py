646c20ae97c4b96209b40ffe37bbec76ae0f9a42
Ben <bray22@illinois.edu>
2025-11-22 11:54:54 -0800

    rem comment

--- pylabrobot/thermocycling/thermo_fisher/atc.py
+++ pylabrobot/thermocycling/thermo_fisher/atc.py
@@ -7,7 +7,7 @@ class ATCBackend(ThermoFisherThermocyclerBackend):
   async def close_lid(self):
     if self.bid != "31":
       raise NotImplementedError("Lid control is only available for BID 31 (ATC)")
-    res = await self.send_command({"cmd": "lidclose"}, response_timeout=20, read_once=True) # 
+    res = await self.send_command({"cmd": "lidclose"}, response_timeout=20, read_once=True)
     if self._parse_scpi_response(res)["status"] != "OK":
       raise ValueError("Failed to close lid")
 
