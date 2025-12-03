0a56d8212abcdac2887e1bc606cf6d08c726d47a
David Nedrud <david.nedrud@gmail.com>
2025-12-03 10:33:38 -0600

    Update module.py
    updated dimensions from opentrons data

--- pylabrobot/resources/opentrons/module.py
+++ pylabrobot/resources/opentrons/module.py
@@ -13,8 +13,8 @@ def Opentrons_deep_well_aluminum_block(name: str) -> PlateHolder:
 
   return PlateHolder(
     name=name,
-    size_x=127.7,  # measured
-    size_y=85.5,  # measured
+    size_x=127.76,  # opentrons data
+    size_y=85.48,  # opentrons data
     size_z=21.5,  # measured
     child_location=Coordinate(0, 0, 5.1),  # measured
     #pedestal_size_z=,
