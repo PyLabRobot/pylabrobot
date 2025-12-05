18032c7cc48150cf23462658d2b09c76dcc3ebf0
David Nedrud <david.nedrud@gmail.com>
2025-12-05 13:52:06 -0600

    Clean up hamilton_mfx_opentrons_module docstring
    Removed commented out resource size variables

--- pylabrobot/resources/diy/davidnedrud/modules.py
+++ pylabrobot/resources/diy/davidnedrud/modules.py
@@ -3,13 +3,10 @@ from pylabrobot.resources.carrier import Coordinate, ResourceHolder
 
 def hamilton_mfx_opentrons_module(name: str) -> ResourceHolder:
   """3D printed part
-  Module to hold a opentrons module on an MFX carrier. Bolts into carrier
+  Module to hold an opentrons module on an MFX carrier. Bolts into carrier
   see STL file for printing
   https://cad.onshape.com/documents/71f70c40910fd15876f75d76/w/81912f5001c1f8dcb28dfd3b/e/da8c964d83d158897c596d21
   """
-
-  # resource_size_x=127.76,
-  # resource_size_y=85.48,
 
   return ResourceHolder(
     name=name,
