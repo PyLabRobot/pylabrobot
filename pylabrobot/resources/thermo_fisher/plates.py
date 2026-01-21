e44d776cc392faf838a84eeff3cade15bf51ea3d
harley-pioneer <harley@pioneer-labs.org>
2026-01-20 16:26:04 -0800

    add plate definition for nalgene reservoir

--- pylabrobot/resources/thermo_fisher/plates.py
+++ pylabrobot/resources/thermo_fisher/plates.py
@@ -421,3 +421,35 @@ def thermo_nunc_1_wellplate_90000uL_Fb_omnitray(name: str) -> Plate:
       # compute_height_from_volume=None,
     ),
   )
+
+
+def Thermo_TS_Nalgene_reservoir_300mL_Fb(name: str) -> Plate:
+  """Thermo Fisher Scientific Nalgene 300mL Flat Bottom Reservoir
+  - Product Number: 12001300 (non-sterile), 12001301 (sterile)
+  - 1-well reservoir with SBS footprint
+  - Max Volume: 300 mL
+  - manufacturer_link: https://www.fishersci.com/shop/products/nalgene-disposable-polypropylene-robotic-reservoirs/12565572
+  - Spec sheet info: https://assets.fishersci.com/TFS-Assets/LCD/Schematics-&-Diagrams/120013XX_0405.PDF
+  """
+  return Plate(
+    name=name,
+    size_x=127.8, #from spec
+    size_y=85.5, #from spec
+    size_z=39.9, #from spec
+    model="Thermo_TS_Nalgene_reservoir_300mL_Fb",
+    ordered_items=create_ordered_items_2d(Well,
+      num_items_x=1, #from spec
+      num_items_y=1, #from spec
+      dx=(127.8-123.8)/2, #from spec
+      dy=(85.5-82.1)/2, #from spec
+      dz=3.3,  #from spec
+      item_dx=0, #from spec
+      item_dy=0, #from spec
+      size_x=123.8, #from spec
+      size_y=82.1, #from spec
+      size_z=39.9, #from spec
+      bottom_type=WellBottomType.FLAT, #from spec
+      cross_section_type=CrossSectionType.RECTANGLE, #rectangle wells
+      material_z_thickness=1.15 #measured.
+    ),
+  )
