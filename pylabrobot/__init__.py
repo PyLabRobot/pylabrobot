9ea315d8567fc722b871c696d99d76014bb93d37
Tiberiu Sabău <T.Sabau@student.tudelft.nl>
2026-01-22 17:39:07 +0100

    add verbose as default jupyter notebooks

--- pylabrobot/__init__.py
+++ pylabrobot/__init__.py
@@ -13,6 +13,21 @@ CONFIG_FILE_NAME = "pylabrobot"
 CONFIG = load_config(CONFIG_FILE_NAME, create_default=False)
 
 
+def _is_running_in_jupyter() -> bool:
+  """
+  Check if the code is running in a Jupyter notebook environment.
+
+  Returns:
+    True if running in Jupyter, False otherwise.
+  """
+  try:
+    # Check if IPython is available and get_ipython() returns a valid instance
+    get_ipython  # type: ignore
+    return True
+  except NameError:
+    return False
+
+
 def project_root() -> Path:
   """
   Get the root directory of the project.
@@ -62,6 +77,9 @@ def setup_logger(log_dir: Optional[Union[Path, str]], level: int):
 def configure(cfg: Config):
   """Configure pylabrobot."""
   setup_logger(cfg.logging.log_dir, cfg.logging.level)
+  # Enable verbose mode by default when running in Jupyter notebook
+  if _is_running_in_jupyter():
+    verbose(True)
 
 
 configure(CONFIG)
