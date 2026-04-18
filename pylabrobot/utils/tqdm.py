3fee2826fccb4ba5dae3e51270e3e6a041eecda4
Camillo Moschner <camillo.moschner@biocam.guide>
2026-04-18 01:35:08 +0100

    `make format`

--- pylabrobot/utils/tqdm.py
+++ pylabrobot/utils/tqdm.py
@@ -58,8 +58,20 @@ class tqdm(Generic[T]):
     total: Optional[int] = None,
     rate: int = 100,
   ):
-    self.iterable, self.disable, self.unit, self.unit_scale, self.rate = iterable, disable, unit, unit_scale, rate
-    self.st, self.i, self.n, self.skip, self.t = time.perf_counter(), -1, 0, 1, getattr(iterable, "__len__", lambda: 0)() if total is None else total
+    self.iterable, self.disable, self.unit, self.unit_scale, self.rate = (
+      iterable,
+      disable,
+      unit,
+      unit_scale,
+      rate,
+    )
+    self.st, self.i, self.n, self.skip, self.t = (
+      time.perf_counter(),
+      -1,
+      0,
+      1,
+      getattr(iterable, "__len__", lambda: 0)() if total is None else total,
+    )
     self.set_description(desc)
     self.update(0)
 
@@ -83,25 +95,54 @@ class tqdm(Generic[T]):
     self.n, self.i = self.n + n, self.i + 1
     if self.disable or (not close and self.i % self.skip != 0):
       return
-    prog, elapsed, ncols = self.n / self.t if self.t else 0, time.perf_counter() - self.st, shutil.get_terminal_size().columns
+    prog, elapsed, ncols = (
+      self.n / self.t if self.t else 0,
+      time.perf_counter() - self.st,
+      shutil.get_terminal_size().columns,
+    )
     if elapsed and self.i / elapsed > self.rate and self.i:
       self.skip = max(int(self.i / elapsed) // self.rate, 1)
 
     def HMS(t):
-      return ":".join(f"{x:02d}" if i else str(x) for i, x in enumerate([int(t) // 3600, int(t) % 3600 // 60, int(t) % 60]) if i or x)
+      return ":".join(
+        f"{x:02d}" if i else str(x)
+        for i, x in enumerate([int(t) // 3600, int(t) % 3600 // 60, int(t) % 60])
+        if i or x
+      )
 
     def SI(x):
       if not x:
         return "0.00"
-      v = f"{x/1000**int(g:=round(math.log(x,1000),6)):.{int(3-3*math.fmod(g,1))}f}"[:4].rstrip(".")
-      return (f"{x/1000**(int(g)+1):.3f}"[:4].rstrip(".") + " kMGTPEZY"[int(g) + 1]) if v == "1000" else v + " kMGTPEZY"[int(g)].strip()
+      v = f"{x / 1000 ** int(g := round(math.log(x, 1000), 6)):.{int(3 - 3 * math.fmod(g, 1))}f}"[
+        :4
+      ].rstrip(".")
+      return (
+        (f"{x / 1000 ** (int(g) + 1):.3f}"[:4].rstrip(".") + " kMGTPEZY"[int(g) + 1])
+        if v == "1000"
+        else v + " kMGTPEZY"[int(g)].strip()
+      )
 
-    prog_text = f'{SI(self.n)}{f"/{SI(self.t)}" if self.t else self.unit}' if self.unit_scale else f'{self.n}{f"/{self.t}" if self.t else self.unit}'
-    est_text = f'<{HMS(elapsed/prog-elapsed) if self.n else "?"}' if self.t else ""
-    it_text = (SI(self.n / elapsed) if self.unit_scale else f"{self.n/elapsed:5.2f}") if self.n else "?"
+    prog_text = (
+      f"{SI(self.n)}{f'/{SI(self.t)}' if self.t else self.unit}"
+      if self.unit_scale
+      else f"{self.n}{f'/{self.t}' if self.t else self.unit}"
+    )
+    est_text = f"<{HMS(elapsed / prog - elapsed) if self.n else '?'}" if self.t else ""
+    it_text = (
+      (SI(self.n / elapsed) if self.unit_scale else f"{self.n / elapsed:5.2f}") if self.n else "?"
+    )
     suf = f"{prog_text} [{HMS(elapsed)}{est_text}, {it_text}{self.unit}/s]"
     sz = max(ncols - len(self.desc) - 3 - 2 - 2 - len(suf), 1)
-    bar = "\r" + self.desc + (f'{100*prog:3.0f}%|{("█"*int(num:=sz*prog)+" ▏▎▍▌▋▊▉"[int(8*num)%8].strip()).ljust(sz," ")}| ' if self.t else "") + suf
+    bar = (
+      "\r"
+      + self.desc
+      + (
+        f"{100 * prog:3.0f}%|{('█' * int(num := sz * prog) + ' ▏▎▍▌▋▊▉'[int(8 * num) % 8].strip()).ljust(sz, ' ')}| "
+        if self.t
+        else ""
+      )
+      + suf
+    )
     print(bar[: ncols + 1], flush=True, end="\n" * close, file=sys.stderr)
 
   @classmethod
