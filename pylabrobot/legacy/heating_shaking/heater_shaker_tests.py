6344107d37a05b4fe2fbf2959dc906c21f116d91
j0nch <jonchen4@u.washington.edu>
2026-06-23 18:07:32 -0700

    Restore legacy LiquidHandler arm resource moves (#1111)

--- pylabrobot/legacy/heating_shaking/heater_shaker_tests.py
+++ pylabrobot/legacy/heating_shaking/heater_shaker_tests.py
@@ -18,3 +18,24 @@ class HeaterShakerTests(unittest.TestCase):
     serialized = hs.serialize()
     deserialized = HeaterShaker.deserialize(serialized)
     self.assertEqual(hs, deserialized)
+
+
+class HeaterShakerSetupTests(unittest.IsolatedAsyncioTestCase):
+  async def test_setup_and_stop(self):
+    hs = HeaterShaker(
+      name="test_hs",
+      size_x=10,
+      size_y=10,
+      size_z=10,
+      backend=HeaterShakerChatterboxBackend(),
+      child_location=Coordinate(0, 0, 0),
+    )
+
+    await hs.setup()
+    try:
+      self.assertTrue(hs.setup_finished)
+      await hs.set_temperature(37)
+      await hs.shake(speed=100, duration=0)
+    finally:
+      await hs.stop()
+    self.assertFalse(hs.setup_finished)
