b7b73f881ce5dbf79860a8fcd9026f3c783b37f3
Rick Wierenga <rick@pylabrobot.org>
2026-09-10 11:17:19 -0700

    Tolerate yaw rounding when placing resources in holders

--- pylabrobot/resources/resource_holder_tests.py
+++ pylabrobot/resources/resource_holder_tests.py
@@ -1,5 +1,6 @@
 import unittest
 
+from .coordinate import Coordinate
 from .resource import Resource
 from .resource_holder import ResourceHolder
 
@@ -14,6 +15,20 @@ class ResourceHolderTests(unittest.TestCase):
     self.holder.resource = self.resource
     self.assertEqual(self.holder.resource, self.resource)
     self.assertEqual(self.resource.parent, self.holder)
+
+  def test_assign_with_yaw_rounding_error(self):
+    self.resource.rotation.z = 269.999999999999
+
+    self.holder.assign_child_resource(self.resource)
+
+    self.assertEqual(self.resource.location, Coordinate(0, 1, 0))
+    self.assertEqual(self.resource.rotation.z, 269.999999999999)
+
+  def test_reject_misaligned_yaw(self):
+    self.resource.rotation.z = 269.99
+
+    with self.assertRaisesRegex(ValueError, "multiple of 90 degrees"):
+      self.holder.assign_child_resource(self.resource)
 
   def test_over_assignment(self):
     self.holder.resource = self.resource
